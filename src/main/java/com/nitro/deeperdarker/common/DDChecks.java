57ff635cd8f5e818a90038fbe97f3fd43be8cdc4
nitrodynamite18 <69542632+nitrodynamite18@users.noreply.github.com>
2022-07-12 13:27:22 -0500

    Updated names

--- src/main/java/com/nitro/deeperdarker/common/DDChecks.java
+++ src/main/java/com/nitro/deeperdarker/common/DDChecks.java
@@ -9,19 +9,19 @@ import net.minecraft.world.item.enchantment.EnchantmentHelper;
 
 public class DDChecks {
 
-    public static boolean isEntityWearingFeatherFall(LivingEntity potentialVictim) {
-        return EnchantmentHelper.getEnchantmentLevel(FALL_PROTECTION, potentialVictim) >= 0;
+    public static boolean hasFeatherFalling(LivingEntity potentialVictim) {
+        return EnchantmentHelper.getEnchantmentLevel(FALL_PROTECTION, potentialVictim) > 0;
     }
 
-    public static boolean doesEntityHaveSlowFall(LivingEntity potentialVictim) {
+    public static boolean hasSlowFall(LivingEntity potentialVictim) {
         return potentialVictim.hasEffect(SLOW_FALLING);
     }
 
-    public static boolean isEntitySculkSafe(LivingEntity potentialVictim) {
+    public static boolean isSculkSafe(LivingEntity potentialVictim) {
         return potentialVictim.getType().is(DDTags.EntityTypes.SCULK_SAFE);
     }
 
     public static boolean doesEntityTriggerSculk(LivingEntity checkEntity) {
-        return DDChecks.isEntityWearingFeatherFall(checkEntity) || DDChecks.doesEntityHaveSlowFall(checkEntity) || DDChecks.isEntitySculkSafe(checkEntity);
+        return DDChecks.hasFeatherFalling(checkEntity) || DDChecks.hasSlowFall(checkEntity) || DDChecks.isSculkSafe(checkEntity);
     }
 }
