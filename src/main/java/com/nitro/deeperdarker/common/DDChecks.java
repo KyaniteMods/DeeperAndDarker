bb2ae4d5968949136f5dde29cf968c924999c61b
nitrodynamite18 <69542632+nitrodynamite18@users.noreply.github.com>
2022-07-22 17:34:29 -0500

    Not going to check for the slow fall effect or feather falling enchantment anymore, so removed the checks for them

--- src/main/java/com/nitro/deeperdarker/common/DDChecks.java
+++ src/main/java/com/nitro/deeperdarker/common/DDChecks.java
@@ -10,19 +10,11 @@ import net.minecraft.world.item.enchantment.EnchantmentHelper;
 
 public class DDChecks {
 
-    public static boolean hasFeatherFalling(LivingEntity potentialVictim) {
-        return EnchantmentHelper.getEnchantmentLevel(FALL_PROTECTION, potentialVictim) > 0;
-    }
-
-    public static boolean hasSlowFall(LivingEntity potentialVictim) {
-        return potentialVictim.hasEffect(SLOW_FALLING);
-    }
-
     public static boolean isSculk(Entity potentialVictim) {
         return potentialVictim.getType().is(DDTags.EntityTypes.SCULK);
     }
 
     public static boolean doesEntityTriggerSculk(LivingEntity checkEntity) {
-        return DDChecks.hasFeatherFalling(checkEntity) || DDChecks.hasSlowFall(checkEntity) || DDChecks.isSculk(checkEntity);
+        return DDChecks.isSculk(checkEntity);
     }
 }
