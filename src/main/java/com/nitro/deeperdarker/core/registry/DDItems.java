e1b08557a656da96dde0893b13af0c022b5a1dcb
nitrodynamite18 <69542632+nitrodynamite18@users.noreply.github.com>
2022-07-26 13:30:01 -0500

    Added Warden Carapace ArmorMaterial and Item

--- src/main/java/com/nitro/deeperdarker/core/registry/DDItems.java
+++ src/main/java/com/nitro/deeperdarker/core/registry/DDItems.java
@@ -3,7 +3,9 @@ package com.nitro.deeperdarker.core.registry;
 import com.mojang.datafixers.util.Pair;
 import com.nitro.deeperdarker.core.DeeperAndDarker;
 import com.teamabnormals.blueprint.core.util.registry.ItemSubRegistryHelper;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
@@ -18,6 +20,10 @@ public class DDItems {
     // Boats
     public static final Pair<RegistryObject<Item>, RegistryObject<Item>> BONE_BOAT = HELPER.createBoatAndChestBoatItem("bone", DDBlocks.BONE_PLANKS);
     public static final Pair<RegistryObject<Item>, RegistryObject<Item>> SCULK_BONE_BOAT = HELPER.createBoatAndChestBoatItem("sculk_bone", DDBlocks.SCULK_BONE_PLANKS);
+
+    // Warden Carapace and armor
+    public static final RegistryObject<Item> WARDEN_CARAPACE = ITEMS.register("warden_carapace", () -> new Item(new Item.Properties().fireResistant().tab(CreativeModeTab.TAB_MATERIALS).rarity(Rarity.EPIC)));
+
 
     // Sculk Reactor Core
 
