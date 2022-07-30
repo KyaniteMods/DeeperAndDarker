f1122fb84631256c3db5eb258d5f345d39ae1749
ang-xd <angelo.barone2010@icloud.com>
2022-07-29 21:11:11 -0400

    Warden helmet effects base (does not work)

--- src/main/java/com/kyanite/deeperdarker/registry/items/SculkArmorItem.java
+++ src/main/java/com/kyanite/deeperdarker/registry/items/SculkArmorItem.java
@@ -1,7 +1,13 @@
 package com.kyanite.deeperdarker.registry.items;
 
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
 import software.bernie.geckolib3.core.builder.AnimationBuilder;
@@ -23,6 +29,15 @@ public class SculkArmorItem extends GeoArmorItem implements IAnimatable {
     public void registerControllers(AnimationData data) {
         data.addAnimationController(new AnimationController<SculkArmorItem>(this, "controller",
                 20, this::predicate));
+    }
+
+    @Override
+    public void onArmorTick(ItemStack stack, Level level, Player player) {
+        if(!level.isClientSide()) {
+            if(player.getInventory().getArmor(EquipmentSlot.HEAD.getIndex()).is(DDItems.WARDEN_HELMET.get())) {
+                player.addEffect(new MobEffectInstance(MobEffects.BLINDNESS, 1));
+            }
+        }
     }
 
     private <P extends IAnimatable> PlayState predicate(AnimationEvent<P> event) {
