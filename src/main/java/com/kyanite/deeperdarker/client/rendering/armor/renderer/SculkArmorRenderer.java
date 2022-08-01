0a8363369b6c007937d9b9ad74961d6021c55ae4
ang-xd <angelo.barone2010@icloud.com>
2022-07-31 20:56:25 -0400

    Fix waist crouching bug

--- src/main/java/com/kyanite/deeperdarker/client/rendering/armor/renderer/SculkArmorRenderer.java
+++ src/main/java/com/kyanite/deeperdarker/client/rendering/armor/renderer/SculkArmorRenderer.java
@@ -7,6 +7,7 @@ import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.world.entity.EquipmentSlot;
 import software.bernie.geckolib3.core.processor.IBone;
 import software.bernie.geckolib3.renderers.geo.GeoArmorRenderer;
+import software.bernie.geckolib3.util.GeoUtils;
 
 public class SculkArmorRenderer extends GeoArmorRenderer<SculkArmorItem> {
     public SculkArmorRenderer() {
@@ -19,6 +20,16 @@ public class SculkArmorRenderer extends GeoArmorRenderer<SculkArmorItem> {
         this.leftLegBone = "armorLeftLeg";
         this.rightBootBone = "armorRightBoot";
         this.leftBootBone = "armorLeftBoot";
+    }
+
+    @Override
+    protected void fitToBiped() {
+        super.fitToBiped();
+        IBone bodyBone = this.getGeoModelProvider().getBone("armorWaist");
+        GeoUtils.copyRotations(this.body, bodyBone);
+        bodyBone.setPositionX(this.body.x);
+        bodyBone.setPositionY(-this.body.y);
+        bodyBone.setPositionZ(this.body.z);
     }
 
     @Override
