58a92a9cbbe90c42684b76a66deb96ffe7cbdc00
ang-xd <angelo.barone2010@icloud.com>
2022-08-03 09:27:37 -0400

    Custom sculk spreading

---
+++ src/main/java/com/kyanite/deeperdarker/client/mixin/SculkSpreadMixin.java
@@ -0,0 +1,77 @@
+package com.kyanite.deeperdarker.client.mixin;
+
+import com.kyanite.deeperdarker.registry.blocks.DDBlocks;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.tags.TagKey;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(SculkVeinBlock.class)
+public abstract class SculkSpreadMixin extends MultifaceBlock {
+    @Shadow @Final private MultifaceSpreader veinSpreader;
+
+    public SculkSpreadMixin(Properties pProperties) {
+        super(pProperties);
+    }
+    @Shadow public abstract void onDischarged(LevelAccessor p_222359_, BlockState p_222360_, BlockPos p_222361_, RandomSource p_222362_);
+
+    @Inject(method = "attemptPlaceSculk", cancellable = true, at = @At("HEAD"))
+    public void attemptSculk(SculkSpreader p_222376_, LevelAccessor p_222377_, BlockPos p_222378_, RandomSource p_222379_, CallbackInfoReturnable<Boolean> cir) {
+        cir.cancel();
+        BlockState blockstate = p_222377_.getBlockState(p_222378_);
+        TagKey<Block> tagkey = p_222376_.replaceableBlocks();
+
+        for(Direction direction : Direction.allShuffled(p_222379_)) {
+            if (hasFace(blockstate, direction)) {
+                BlockPos blockpos = p_222378_.relative(direction);
+                BlockState blockstate1 = p_222377_.getBlockState(blockpos);
+                if (blockstate1.is(tagkey)) {
+                    BlockState blockstate2 = Blocks.SCULK.defaultBlockState();
+                    if(blockstate1.is(Blocks.COBBLESTONE))
+                        blockstate2 = DDBlocks.COBBLED_SCULK_STONE.get().defaultBlockState();
+                    else if(blockstate2.is(Blocks.DEEPSLATE) || blockstate1.is(Blocks.STONE))
+                        blockstate2 = DDBlocks.SCULK_STONE.get().defaultBlockState();
+                    else if(blockstate2.is(Blocks.BONE_BLOCK))
+                        blockstate2 = DDBlocks.SCULK_BONE_BLOCK.get().defaultBlockState();
+
+                    p_222377_.setBlock(blockpos, blockstate2, 3);
+                    Block.pushEntitiesUp(blockstate1, blockstate2, p_222377_, blockpos);
+                    p_222377_.playSound((Player)null, blockpos, SoundEvents.SCULK_BLOCK_SPREAD, SoundSource.BLOCKS, 1.0F, 1.0F);
+                    this.veinSpreader.spreadAll(blockstate2, p_222377_, blockpos, p_222376_.isWorldGeneration());
+                    Direction direction1 = direction.getOpposite();
+
+                    for(Direction direction2 : DIRECTIONS) {
+                        if (direction2 != direction1) {
+                            BlockPos blockpos1 = blockpos.relative(direction2);
+                            BlockState blockstate3 = p_222377_.getBlockState(blockpos1);
+                            if (blockstate3.is(this)) {
+                                this.onDischarged(p_222377_, blockstate3, blockpos1, p_222379_);
+                            }
+                        }
+                    }
+
+                    cir.setReturnValue(true);
+                }
+            }
+        }
+
+        cir.setReturnValue(false);
+    }
+
+    @Override
+    public MultifaceSpreader getSpreader() {
+        return veinSpreader;
+    }
+}
