08e4a0cc9423c63fa3a8a2e763dbb9ab87a3b075
muhta <muhtasim.nabi@gmail.com>
2022-08-04 23:47:40 -0500

    fixed sculk leech hitbox

--- src/main/java/com/kyanite/deeperdarker/registry/entities/DDEntities.java
+++ src/main/java/com/kyanite/deeperdarker/registry/entities/DDEntities.java
@@ -17,6 +17,6 @@ public class DDEntities {
 
     public static final RegistryObject<EntityType<SculkWormEntity>> SCULK_WORM = ENTITY_TYPES.register("shriek_worm", () -> EntityType.Builder.of(SculkWormEntity::new, MobCategory.MONSTER).sized(1.5f, 4.5F).clientTrackingRange(10).build(new ResourceLocation(DeeperAndDarker.MOD_ID, "shriek_worm").toString()));
     public static final RegistryObject<EntityType<SculkSnapperEntity>> SCULK_SNAPPER = ENTITY_TYPES.register("sculk_snapper", () -> EntityType.Builder.of(SculkSnapperEntity::new, MobCategory.MONSTER).sized(1, 1).build(new ResourceLocation(DeeperAndDarker.MOD_ID, "sculk_snapper").toString()));
-    public static final RegistryObject<EntityType<SculkLeechEntity>> SCULK_LEECH = ENTITY_TYPES.register("sculk_leech", () -> EntityType.Builder.of(SculkLeechEntity::new, MobCategory.MONSTER).sized(0.f, 0.3f).build(new ResourceLocation(DeeperAndDarker.MOD_ID, "sculk_leech").toString()));
+    public static final RegistryObject<EntityType<SculkLeechEntity>> SCULK_LEECH = ENTITY_TYPES.register("sculk_leech", () -> EntityType.Builder.of(SculkLeechEntity::new, MobCategory.MONSTER).sized(0.3f, 0.2f).build(new ResourceLocation(DeeperAndDarker.MOD_ID, "sculk_leech").toString()));
     public static final RegistryObject<EntityType<ShatteredEntity>> SHATTERED = ENTITY_TYPES.register("shattered", () -> EntityType.Builder.of(ShatteredEntity::new, MobCategory.MONSTER).sized(0.85f, 1.4F).build(new ResourceLocation(DeeperAndDarker.MOD_ID, "shattered").toString()));
 }
