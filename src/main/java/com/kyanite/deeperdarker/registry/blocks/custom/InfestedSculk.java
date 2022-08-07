546bddbc6e0b7d810cbf3bdadfb7553d3193cea7
muhta <muhtasim.nabi@gmail.com>
2022-08-07 15:24:56 -0500

    removed `DDGenericUtils` (all methods are in InfestedSculk class now)

--- src/main/java/com/kyanite/deeperdarker/registry/blocks/custom/InfestedSculk.java
+++ src/main/java/com/kyanite/deeperdarker/registry/blocks/custom/InfestedSculk.java
@@ -2,7 +2,6 @@ package com.kyanite.deeperdarker.registry.blocks.custom;
 
 import com.kyanite.deeperdarker.registry.entities.DDEntities;
 import com.kyanite.deeperdarker.registry.entities.custom.SculkWormEntity;
-import com.kyanite.deeperdarker.util.DDGenericUtils;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
@@ -10,6 +9,7 @@ import net.minecraft.world.entity.SpawnPlacements;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SculkBlock;
 import net.minecraft.world.level.block.state.BlockState;
@@ -25,15 +25,6 @@ public class InfestedSculk extends SculkBlock {
     }
 
     @Override
-    public boolean isValidSpawn(BlockState state, BlockGetter level, BlockPos pos, SpawnPlacements.Type type, EntityType<?> entityType) {
-        if(!DDGenericUtils.isBlockBeside(level, this, pos) &&
-                !DDGenericUtils.noSpace(level, pos, 6))
-            return true;
-
-        return super.isValidSpawn(state, level, pos, type, entityType);
-    }
-
-    @Override
     public void stepOn(Level pLevel, BlockPos pPos, BlockState pState, Entity pEntity) {
         if(pEntity instanceof Player player) {
             pLevel.setBlock(pPos, Blocks.SCULK.defaultBlockState(), 3);
@@ -46,5 +37,22 @@ public class InfestedSculk extends SculkBlock {
         }
 
         super.stepOn(pLevel, pPos, pState, pEntity);
+    }
+
+    @Override
+    public boolean isValidSpawn(BlockState state, BlockGetter level, BlockPos pos, SpawnPlacements.Type type, EntityType<?> entityType) {
+        if(!isBlockBeside(level, this, pos) && !noSpace(level, pos, 6)) return true;
+        return super.isValidSpawn(state, level, pos, type, entityType);
+    }
+
+    public boolean isBlockBeside(BlockGetter level, Block targetBlock, BlockPos origin) {
+        return level.getBlockState(origin.north()).is(targetBlock) || level.getBlockState(origin.east()).is(targetBlock) || level.getBlockState(origin.south()).is(targetBlock) || level.getBlockState(origin.west()).is(targetBlock);
+    }
+
+    public boolean noSpace(BlockGetter getter, BlockPos origin, int distance) {
+        for(int i = 0; i < distance; i++) {
+            if(!getter.getBlockState(origin.above(i)).is(Blocks.AIR)) return true;
+        }
+        return false;
     }
 }
