65602fb99f25377958a891a8384a3659e0b3fb94
muhta <muhtasim.nabi@gmail.com>
2022-08-11 21:39:54 -0500

    fixed warden armor and darkness effect applying in the Otherside issue (i think)

--- src/main/java/com/kyanite/deeperdarker/registry/items/custom/WardenArmorItem.java
+++ src/main/java/com/kyanite/deeperdarker/registry/items/custom/WardenArmorItem.java
@@ -29,20 +29,13 @@ public class WardenArmorItem extends GeoArmorItem implements IAnimatable {
     @Override
     public void onArmorTick(ItemStack stack, Level level, Player player) {
         if(!level.isClientSide()) {
-            if(player.getInventory().getArmor(EquipmentSlot.LEGS.getIndex()).is(DDItems.WARDEN_LEGGINGS.get())) {
-                player.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 25, 0, true, false));
-            }
-
             if(player.getInventory().getArmor(EquipmentSlot.HEAD.getIndex()).is(DDItems.WARDEN_HELMET.get())) {
                 if(player.hasEffect(MobEffects.BLINDNESS)) player.removeEffect(MobEffects.BLINDNESS);
+                if(player.hasEffect(MobEffects.DARKNESS)) player.removeEffect(MobEffects.DARKNESS);
+            }
 
-                if(player.level.dimension() == Level.OVERWORLD) {
-                    if(player.hasEffect(MobEffects.DARKNESS)) player.removeEffect(MobEffects.DARKNESS);
-                }
-
-                if(player.level.dimension() == DDDimensions.OTHERSIDE_LEVEL) {
-                    player.addEffect(new MobEffectInstance(MobEffects.DARKNESS, 25, 0, true, false));
-                }
+            if(player.getInventory().getArmor(EquipmentSlot.LEGS.getIndex()).is(DDItems.WARDEN_LEGGINGS.get())) {
+                player.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 25, 0, true, false));
             }
         }
     }
