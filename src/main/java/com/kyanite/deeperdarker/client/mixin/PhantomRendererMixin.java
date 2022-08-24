e14ccf844a51cc1da55fc9a637091fbbddda73f8
ang-xd <angelo.barone2010@icloud.com>
2022-08-24 17:49:56 -0400

    Sorry im dum

--- src/main/java/com/kyanite/deeperdarker/client/mixin/PhantomRendererMixin.java
+++ src/main/java/com/kyanite/deeperdarker/client/mixin/PhantomRendererMixin.java
@@ -17,6 +17,6 @@ import java.util.Objects;
 public class PhantomRendererMixin {
     @Inject(method = "getTextureLocation(Lnet/minecraft/world/entity/monster/Phantom;)Lnet/minecraft/resources/ResourceLocation;", at = @At("HEAD"), cancellable = true)
     public void getTextureLocation(Phantom pEntity, CallbackInfoReturnable<ResourceLocation> cir) {
-        if(pEntity.level.dimension() == DDDimensions.OTHERSIDE_LEVEL) cir.setReturnValue(new ResourceLocation(DeeperAndDarker.MOD_ID, "textures/entity/sculk_phantom.png");
+        if(pEntity.level.dimension() == DDDimensions.OTHERSIDE_LEVEL) cir.setReturnValue(new ResourceLocation(DeeperAndDarker.MOD_ID, "textures/entity/sculk_phantom.png"));
     }
 }
