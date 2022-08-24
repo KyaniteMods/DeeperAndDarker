7cbd6f940b3bd52b91f7478e7d04049b0c90e11e
nitrodynamite18 <69542632+nitrodynamite18@users.noreply.github.com>
2022-08-24 16:19:58 -0400

    Ancient and Trapped Vases can now be waterlogged.

--- src/main/java/com/kyanite/deeperdarker/registry/blocks/custom/AncientVaseBlock.java
+++ src/main/java/com/kyanite/deeperdarker/registry/blocks/custom/AncientVaseBlock.java
@@ -5,11 +5,19 @@ import com.kyanite.deeperdarker.registry.entities.custom.SculkLeechEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.util.RandomSource;
 import net.minecraft.util.valueproviders.ConstantInt;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.DropExperienceBlock;
+import net.minecraft.world.level.block.SimpleWaterloggedBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.phys.shapes.BooleanOp;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
@@ -17,10 +25,13 @@ import net.minecraft.world.phys.shapes.VoxelShape;
 
 import java.util.stream.Stream;
 
-public class AncientVaseBlock extends DropExperienceBlock {
+public class AncientVaseBlock extends DropExperienceBlock implements SimpleWaterloggedBlock {
     public final RandomSource randomSource = RandomSource.create();
-    public AncientVaseBlock(Properties pProperties) {
-        super(pProperties, ConstantInt.of(1));
+    public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
+
+    public AncientVaseBlock(Properties properties) {
+        super(properties, ConstantInt.of(1));
+        this.registerDefaultState(this.defaultBlockState().setValue(WATERLOGGED, Boolean.FALSE));
     }
 
     @Override
@@ -30,6 +41,20 @@ public class AncientVaseBlock extends DropExperienceBlock {
                 Block.box(3, 0, 3, 13, 1, 13),
                 Block.box(4, 13, 4, 12, 16, 12)
         ).reduce((v1, v2) -> Shapes.join(v1, v2, BooleanOp.OR)).get();
+    }
+
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        LevelAccessor levelaccessor = context.getLevel();
+        BlockPos blockpos = context.getClickedPos();
+        return this.defaultBlockState().setValue(WATERLOGGED, levelaccessor.getFluidState(blockpos).getType() == Fluids.WATER);
+    }
+
+    public FluidState getFluidState(BlockState state) {
+        return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
+    }
+
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(WATERLOGGED);
     }
 
     @Override
--- src/main/java/com/kyanite/deeperdarker/registry/blocks/custom/TrappedVaseBlock.java
+++ src/main/java/com/kyanite/deeperdarker/registry/blocks/custom/TrappedVaseBlock.java
@@ -1,14 +1,14 @@
 package com.kyanite.deeperdarker.registry.blocks.custom;
 
-import com.kyanite.deeperdarker.registry.entities.DDEntities;
 import com.kyanite.deeperdarker.registry.entities.custom.StalkerEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.SimpleWaterloggedBlock;
 import net.minecraft.world.level.block.state.BlockState;
 
-public class TrappedVaseBlock extends AncientVaseBlock {
-    public TrappedVaseBlock(Properties pProperties) {
-        super(pProperties);
+public class TrappedVaseBlock extends AncientVaseBlock implements SimpleWaterloggedBlock {
+    public TrappedVaseBlock(Properties properties) {
+        super(properties);
     }
 
     @Override
