b6ac24c3e269dd4bd91ed7c215a86bdd2dfa36f8
muhta <muhtasim.nabi@gmail.com>
2022-08-25 20:30:20 -0500

    events optimizing

--- src/main/java/com/kyanite/deeperdarker/DeeperAndDarker.java
+++ src/main/java/com/kyanite/deeperdarker/DeeperAndDarker.java
@@ -16,6 +16,7 @@ import com.kyanite.deeperdarker.registry.potions.DDPotions;
 import com.kyanite.deeperdarker.registry.sounds.DDSounds;
 import com.kyanite.deeperdarker.registry.world.biomemodifiers.DDBiomeModifiers;
 import com.kyanite.deeperdarker.registry.world.biomes.OthersideBiomes;
+import com.kyanite.deeperdarker.registry.world.dimension.DDDimensions;
 import com.kyanite.deeperdarker.registry.world.dimension.DDPoiTypes;
 import com.kyanite.deeperdarker.registry.world.features.DDConfiguredFeatures;
 import com.kyanite.deeperdarker.registry.world.features.DDFeatures;
@@ -25,6 +26,9 @@ import net.minecraft.client.renderer.Sheets;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
 import net.minecraft.client.renderer.blockentity.SignRenderer;
 import net.minecraft.client.renderer.entity.EntityRenderers;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.SpawnPlacements;
 import net.minecraft.world.item.alchemy.PotionBrewing;
@@ -33,9 +37,9 @@ import net.minecraft.world.level.block.ComposterBlock;
 import net.minecraft.world.level.block.state.properties.WoodType;
 import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -79,10 +83,9 @@ public class DeeperAndDarker {
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
-    public static class DeeperAndDarkerClient {
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class DeeperDarkerClient {
         @SubscribeEvent
-        @OnlyIn(Dist.CLIENT)
         public static void clientSetup(final FMLClientSetupEvent event) {
             WoodType.register(DDWoodTypes.ECHO);
             BlockEntityRenderers.register(DDBlockEntityTypes.SIGN_BLOCK_ENTITIES.get(), SignRenderer::new);
@@ -95,18 +98,19 @@ public class DeeperAndDarker {
         }
 
         @SubscribeEvent
-        @OnlyIn(Dist.CLIENT)
         public static void registerRenderers(final EntityRenderersEvent.RegisterRenderers event) {
             event.registerEntityRenderer(DDEntities.BOAT.get(), context -> new DDBoatRenderer<>(context, false));
             event.registerEntityRenderer(DDEntities.CHEST_BOAT.get(), context -> new DDBoatRenderer<>(context, true));
         }
 
         @SubscribeEvent
-        @OnlyIn(Dist.CLIENT)
         public static void entityRenderers(final EntityRenderersEvent.AddLayers event) {
             GeoArmorRenderer.registerArmorRenderer(WardenArmorItem.class, WardenArmorRenderer::new);
         }
+    }
 
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class DeeperDarkerCommon {
         @SubscribeEvent
         public static void commonSetup(final FMLCommonSetupEvent event) {
             event.enqueueWork(() -> {
@@ -133,6 +137,20 @@ public class DeeperAndDarker {
             event.put(DDEntities.SCULK_LEECH.get(), SculkLeechEntity.attributes());
             event.put(DDEntities.SHATTERED.get(), ShatteredEntity.attributes());
             event.put(DDEntities.STALKER.get(), StalkerEntity.attributes());
+        }
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID)
+    public static class DeeperDarkerEvents {
+        @SubscribeEvent
+        public static void playerTick(final TickEvent.PlayerTickEvent event) {
+            if(!event.player.level.isClientSide()) {
+                if(event.player.getLevel().dimension() == DDDimensions.OTHERSIDE_LEVEL) {
+                    if(!event.player.getInventory().getArmor(EquipmentSlot.HEAD.getIndex()).is(DDItems.WARDEN_HELMET.get()) && !event.player.isCreative() && !event.player.isSpectator()) {
+                        event.player.addEffect(new MobEffectInstance(MobEffects.DARKNESS, 25, 0));
+                    }
+                }
+            }
         }
     }
 }
