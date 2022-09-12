455dc14be3b4ebb67c819bdd482d860cb30dc208
muhta <muhtasim.nabi@gmail.com>
2022-09-11 19:52:00 -0500

    sculk smite now has 5 levels and is no longer compatible with sharpness, smite, and bane of arthropods

--- src/main/java/com/kyanite/deeperdarker/registry/enchantments/SculkDamageEnchantment.java
+++ src/main/java/com/kyanite/deeperdarker/registry/enchantments/SculkDamageEnchantment.java
@@ -3,22 +3,34 @@ package com.kyanite.deeperdarker.registry.enchantments;
 import com.kyanite.deeperdarker.miscellaneous.DDTypes;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.MobType;
+import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.DamageEnchantment;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
 
 public class SculkDamageEnchantment extends Enchantment {
-    public SculkDamageEnchantment(Rarity pRarity, EnchantmentCategory pCategory, EquipmentSlot[] pApplicableSlots) {
+    public SculkDamageEnchantment(Rarity pRarity, EnchantmentCategory pCategory, EquipmentSlot... pApplicableSlots) {
         super(pRarity, pCategory, pApplicableSlots);
     }
 
     @Override
-    public float getDamageBonus(int level, MobType mobType, ItemStack enchantedItem) {
-        return mobType == DDTypes.SCULK ? 10F : 0.0F;
+    public int getMaxLevel() {
+        return 5;
     }
 
     @Override
-    public int getMaxLevel() {
-        return 1;
+    public float getDamageBonus(int level, MobType mobType, ItemStack enchantedItem) {
+        return mobType == DDTypes.SCULK ? level * 2.5f : 0;
+    }
+
+    @Override
+    protected boolean checkCompatibility(Enchantment pOther) {
+        return !(pOther instanceof DamageEnchantment);
+    }
+
+    @Override
+    public boolean canEnchant(ItemStack pStack) {
+        return pStack.getItem() instanceof AxeItem || super.canEnchant(pStack);
     }
 }
