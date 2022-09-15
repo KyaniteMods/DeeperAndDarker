9ac3032541d6c48b8cbbce04c4218446410c52a8
nitro_dynamite18 <69542632+nitrodynamite18@users.noreply.github.com>
2022-09-15 19:15:32 -0400

    Fixes issue #69.

--- src/main/java/com/kyanite/deeperdarker/mixin/WardenMixin.java
+++ src/main/java/com/kyanite/deeperdarker/mixin/WardenMixin.java
@@ -1,13 +1,14 @@
 package com.kyanite.deeperdarker.mixin;
 
 import com.kyanite.deeperdarker.miscellaneous.DDTypes;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.MobType;
+import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.monster.warden.Warden;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -27,5 +28,17 @@ public abstract class WardenMixin extends Monster {
     @Inject(method = "createAttributes", at = @At("RETURN"), cancellable = true)
     private static void createAttributes(CallbackInfoReturnable<AttributeSupplier.Builder> cir) {
         cir.setReturnValue(Monster.createMonsterAttributes().add(Attributes.MAX_HEALTH, 350).add(Attributes.MOVEMENT_SPEED, 0.3F).add(Attributes.KNOCKBACK_RESISTANCE, 1.0D).add(Attributes.ATTACK_KNOCKBACK, 1.5D).add(Attributes.ATTACK_DAMAGE, 15));
+    }
+
+    @Inject(method = "canTargetEntity", at = @At("HEAD"), cancellable = true)
+    @Contract("null -> false")
+    public void canTargetEntity(@Nullable Entity entity, CallbackInfoReturnable<Boolean> cir) {
+        if (entity instanceof LivingEntity livingentity) {
+            if (this.level == livingentity.level && EntitySelector.NO_CREATIVE_OR_SPECTATOR.test(livingentity) && !this.isAlliedTo(livingentity) && livingentity.getType() != EntityType.ARMOR_STAND && livingentity.getType() != EntityType.WARDEN && !livingentity.isInvulnerable() && !livingentity.isDeadOrDying() && this.level.getWorldBorder().isWithinBounds(livingentity.getBoundingBox()) && livingentity.getMobType() != DDTypes.SCULK) {
+                cir.setReturnValue(true);
+            }
+        }
+
+        cir.setReturnValue(false);
     }
 }
