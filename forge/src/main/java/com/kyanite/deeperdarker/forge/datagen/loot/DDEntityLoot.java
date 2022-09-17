f7ecc3a27e8e260f8eaf8d2476d39d709e6dd195
angxl wtf <angelo.barone2010@icloud.com>
2022-09-17 10:23:08 -0400

    Datagen

---
+++ forge/src/main/java/com/kyanite/deeperdarker/forge/datagen/loot/DDEntityLoot.java
@@ -0,0 +1,34 @@
+package com.kyanite.deeperdarker.forge.datagen.loot;
+
+import com.kyanite.deeperdarker.registry.entities.DDEntities;
+import com.kyanite.deeperdarker.registry.items.DDItems;
+import net.minecraft.data.loot.EntityLoot;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.level.storage.loot.LootPool;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.predicates.LootItemRandomChanceWithLootingCondition;
+import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
+import net.minecraftforge.registries.RegistryObject;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Iterator;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public class DDEntityLoot extends EntityLoot {
+    @Override
+    protected void addTables() {
+        add(DDEntities.SCULK_LEECH.get(), LootTable.lootTable());
+        add(DDEntities.SCULK_SNAPPER.get(), LootTable.lootTable());
+        add(DDEntities.SCULK_WORM.get(), LootTable.lootTable());
+        add(DDEntities.SHATTERED.get(), LootTable.lootTable().withPool(LootPool.lootPool().setRolls(ConstantValue.exactly(1)).add(LootItem.lootTableItem(DDItems.SOUL_DUST.get())).when(LootItemRandomChanceWithLootingCondition.randomChanceAndLootingBoost(0.25f, 0.0375f))));
+    }
+
+    @NotNull
+    @Override
+    protected Iterable<EntityType<?>> getKnownEntities() {
+        Stream<? extends EntityType<?>> stream = DDEntities.ENTITIES.values().stream().map(Supplier::get);
+        return () -> (Iterator<EntityType<?>>) stream.iterator();
+    }
+}
---
+++ forge/src/main/java/com/kyanite/deeperdarker/forge/datagen/models/DDBlockStateProvider.java
@@ -0,0 +1,103 @@
+package com.kyanite.deeperdarker.forge.datagen.models;
+
+import com.kyanite.deeperdarker.DeeperAndDarker;
+import com.kyanite.deeperdarker.registry.blocks.DDBlocks;
+import com.kyanite.deeperdarker.registry.blocks.custom.SculkJawBlock;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.*;
+import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.client.model.generators.ConfiguredModel;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.Supplier;
+
+public class DDBlockStateProvider extends BlockStateProvider {
+    public DDBlockStateProvider(DataGenerator pGenerator, ExistingFileHelper pExistingFileHelper) {
+        super(pGenerator, DeeperAndDarker.MOD_ID, pExistingFileHelper);
+    }
+
+    @Override
+    protected void registerStatesAndModels() {
+        simpleBlock(DDBlocks.ECHO_PLANKS.get());
+
+        axisBlock(DDBlocks.ECHO_LOG.get(), blockLoc(DDBlocks.ECHO_LOG), blockLoc(DDBlocks.ECHO_LOG, "top"));
+        axisBlock(DDBlocks.STRIPPED_ECHO_LOG.get(), blockLoc(DDBlocks.STRIPPED_ECHO_LOG), blockLoc(DDBlocks.STRIPPED_ECHO_LOG, "top"));
+        axisBlock(DDBlocks.STRIPPED_ECHO_WOOD.get(), models().cubeColumn(DDBlocks.STRIPPED_ECHO_WOOD.get().getDescriptionId(), blockLoc(DDBlocks.STRIPPED_ECHO_LOG), blockLoc(DDBlocks.STRIPPED_ECHO_LOG)), models().cubeColumn(DDBlocks.STRIPPED_ECHO_WOOD.get().getDescriptionId(), blockLoc(DDBlocks.STRIPPED_ECHO_LOG), blockLoc(DDBlocks.STRIPPED_ECHO_LOG)));
+        axisBlock(DDBlocks.ECHO_WOOD.get(), models().cubeColumn(DDBlocks.ECHO_WOOD.get().getDescriptionId(), blockLoc(DDBlocks.ECHO_LOG), blockLoc(DDBlocks.ECHO_LOG)), models().cubeColumn(DDBlocks.ECHO_WOOD.get().getDescriptionId(), blockLoc(DDBlocks.ECHO_LOG), blockLoc(DDBlocks.ECHO_LOG)));
+        simpleBlock(DDBlocks.ECHO_LEAVES.get());
+        slabBlock(DDBlocks.ECHO_SLAB.get(), blockLoc(DDBlocks.ECHO_PLANKS), blockLoc(DDBlocks.ECHO_PLANKS));
+        fenceBlock(DDBlocks.ECHO_FENCE, blockLoc(DDBlocks.ECHO_PLANKS));
+        stairsBlock(DDBlocks.ECHO_STAIRS.get(), blockLoc(DDBlocks.ECHO_PLANKS));
+        buttonBlock(DDBlocks.ECHO_BUTTON, blockLoc(DDBlocks.ECHO_PLANKS));
+        pressurePlateBlock(DDBlocks.ECHO_PRESSURE_PLATE.get(), blockLoc(DDBlocks.ECHO_PLANKS));
+        doorBlockWithRenderType(DDBlocks.ECHO_DOOR.get(), blockLoc(DDBlocks.ECHO_DOOR, "bottom"), blockLoc(DDBlocks.ECHO_DOOR, "top"), "cutout");
+        trapdoorBlockWithRenderType(DDBlocks.ECHO_TRAPDOOR.get(), blockLoc(DDBlocks.ECHO_TRAPDOOR), true, "cutout");
+        fenceGateBlock(DDBlocks.ECHO_FENCE_GATE.get(), blockLoc(DDBlocks.ECHO_PLANKS));
+        signBlock(DDBlocks.ECHO_SIGN.get(), (WallSignBlock) DDBlocks.ECHO_WALL_SIGN.get(), blockLoc(DDBlocks.ECHO_PLANKS));
+
+        simpleBlock(DDBlocks.SCULK_STONE.get());
+        slabBlock(DDBlocks.SCULK_STONE_SLAB.get(), blockLoc(DDBlocks.SCULK_STONE), blockLoc(DDBlocks.SCULK_STONE));
+        stairsBlock(DDBlocks.SCULK_STONE_STAIRS.get(), blockLoc(DDBlocks.SCULK_STONE));
+        wallBlock(DDBlocks.SCULK_STONE_WALL, blockLoc(DDBlocks.SCULK_STONE));
+
+        simpleBlock(DDBlocks.COBBLED_SCULK_STONE.get());
+        slabBlock(DDBlocks.COBBLED_SCULK_STONE_SLAB.get(), blockLoc(DDBlocks.COBBLED_SCULK_STONE), blockLoc(DDBlocks.COBBLED_SCULK_STONE));
+        stairsBlock(DDBlocks.COBBLED_SCULK_STONE_STAIRS.get(), blockLoc(DDBlocks.COBBLED_SCULK_STONE));
+        wallBlock(DDBlocks.COBBLED_SCULK_STONE_WALL, blockLoc(DDBlocks.COBBLED_SCULK_STONE));
+
+        simpleBlock(DDBlocks.POLISHED_SCULK_STONE.get());
+        slabBlock(DDBlocks.POLISHED_SCULK_STONE_SLAB.get(), blockLoc(DDBlocks.POLISHED_SCULK_STONE), blockLoc(DDBlocks.POLISHED_SCULK_STONE));
+        stairsBlock(DDBlocks.POLISHED_SCULK_STONE_STAIRS.get(), blockLoc(DDBlocks.POLISHED_SCULK_STONE));
+        wallBlock(DDBlocks.POLISHED_SCULK_STONE_WALL, blockLoc(DDBlocks.POLISHED_SCULK_STONE));
+
+        simpleBlock(DDBlocks.SCULK_STONE_BRICKS.get());
+        slabBlock(DDBlocks.SCULK_STONE_BRICK_SLAB.get(), blockLoc(DDBlocks.SCULK_STONE_BRICKS), blockLoc(DDBlocks.SCULK_STONE_BRICKS));
+        stairsBlock(DDBlocks.SCULK_STONE_BRICK_STAIRS.get(), blockLoc(DDBlocks.SCULK_STONE_BRICKS));
+        wallBlock(DDBlocks.SCULK_STONE_BRICK_WALL, blockLoc(DDBlocks.SCULK_STONE_BRICKS));
+
+        simpleBlock(DDBlocks.SCULK_STONE_COAL_ORE.get());
+        simpleBlock(DDBlocks.SCULK_STONE_IRON_ORE.get());
+        simpleBlock(DDBlocks.SCULK_STONE_COPPER_ORE.get());
+        simpleBlock(DDBlocks.SCULK_STONE_GOLD_ORE.get());
+        simpleBlock(DDBlocks.SCULK_STONE_REDSTONE_ORE.get());
+        simpleBlock(DDBlocks.SCULK_STONE_EMERALD_ORE.get());
+        simpleBlock(DDBlocks.SCULK_STONE_LAPIS_ORE.get());
+        simpleBlock(DDBlocks.SCULK_STONE_DIAMOND_ORE.get());
+
+        simpleBlock(DDBlocks.SCULK_VINES.get(), models().cross(DDBlocks.SCULK_VINES.get().getDescriptionId(), blockLoc(DDBlocks.SCULK_VINES)).renderType("cutout"));
+        simpleBlock(DDBlocks.SCULK_VINES_PLANT.get(), models().cross(DDBlocks.SCULK_VINES_PLANT.get().getDescriptionId(), blockLoc(DDBlocks.SCULK_VINES_PLANT)).renderType("cutout"));
+        simpleBlock(DDBlocks.ECHO_SOIL.get());
+        simpleBlock(DDBlocks.SCULK_GLEAM.get());
+        simpleBlock(DDBlocks.INFESTED_SCULK.get());
+
+        ConfiguredModel[] sculkJawModels = { new ConfiguredModel(models().cubeAll(DDBlocks.SCULK_JAW.get().getDescriptionId() + "_activated", blockLoc(DDBlocks.SCULK_JAW, "activated"))), new ConfiguredModel(models().cubeAll(DDBlocks.SCULK_JAW.get().getDescriptionId(), blockLoc(DDBlocks.SCULK_JAW))) };
+        getVariantBuilder(DDBlocks.SCULK_JAW.get()).partialState().with(SculkJawBlock.ACTIVATED, true).setModels(sculkJawModels[0]).partialState().with(SculkJawBlock.ACTIVATED, false).setModels(sculkJawModels[1]);
+
+        simpleBlock(DDBlocks.ANCIENT_VASE.get(), models().withExistingParent(DDBlocks.ANCIENT_VASE.get().getDescriptionId(), modLoc("block/vase")).texture("vase", blockLoc(DDBlocks.ANCIENT_VASE)));
+    }
+
+    public void fenceBlock(Supplier<FenceBlock> block, ResourceLocation texture) {
+        super.fenceBlock(block.get(), texture);
+        models().fenceInventory(block.get().getDescriptionId() + "_inventory", texture);
+    }
+
+    public void buttonBlock(Supplier<ButtonBlock> block, ResourceLocation texture) {
+        super.buttonBlock(block.get(), texture);
+        models().buttonInventory(block.get().getDescriptionId() + "_inventory", texture);
+    }
+
+    public void wallBlock(Supplier<WallBlock> block, ResourceLocation texture) {
+        super.wallBlock(block.get(), texture);
+        models().wallInventory(block.get().getDescriptionId() + "_inventory", texture);
+    }
+
+    public ResourceLocation blockLoc(Supplier<? extends Block> block) {
+        return new ResourceLocation(DeeperAndDarker.MOD_ID, "block/" + block.get().getDescriptionId());
+    }
+
+    public ResourceLocation blockLoc(Supplier<? extends Block> block, String suffix) {
+        return new ResourceLocation(DeeperAndDarker.MOD_ID, "block/" + block.get().getDescriptionId() + "_" + suffix);
+    }
+}
