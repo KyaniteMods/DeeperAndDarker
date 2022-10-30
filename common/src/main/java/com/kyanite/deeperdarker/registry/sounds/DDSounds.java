54a2645cb517818eb776310d44c3b1b8ed23c3db
ang-xd <angxdxdd@gmail.com>
2022-10-30 15:25:43 -0400

    Update Paragon on Forge and add Overcast Columns music

--- common/src/main/java/com/kyanite/deeperdarker/registry/sounds/DDSounds.java
+++ common/src/main/java/com/kyanite/deeperdarker/registry/sounds/DDSounds.java
@@ -41,6 +41,7 @@ public class DDSounds {
     // Dimension
     public static final Supplier<SoundEvent> DEEPLANDS_AMBIENCE = register("ambience.deeplands");
     public static final Supplier<SoundEvent> FOREST_AMBIENCE = register("ambience.forest");
+    public static final Supplier<SoundEvent> OVERCAST_AMBIENCE = register("ambience.overcast");
 
     // Item
     public static final Supplier<SoundEvent> SCULK_LINK = register("item.transmitter.link");
