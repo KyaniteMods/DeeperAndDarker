983ad36894905ea959a3b08a364ffe256443167e
nitrodynamite18 <69542632+nitrodynamite18@users.noreply.github.com>
2022-12-10 16:54:17 -0500

    Disabled start screen entirely.

--- common/src/main/java/com/kyanite/deeperdarker/config/DDClientConfig.java
+++ common/src/main/java/com/kyanite/deeperdarker/config/DDClientConfig.java
@@ -1,11 +1,9 @@
 package com.kyanite.deeperdarker.config;
 
-import com.kyanite.paragon.api.ConfigOption;
 import com.kyanite.paragon.api.enums.ConfigSide;
 import com.kyanite.paragon.api.interfaces.Config;
 
 public class DDClientConfig implements Config {
-    public static final ConfigOption<Boolean> SHOW_START_SCREEN = new ConfigOption<>("show_start_screen", false);
 
     @Override
     public ConfigSide configSide() {
