d14408c7b281a44d6945dd16533c7c9a2b450f20
muhta <muhtasim.nabi@gmail.com>
2023-06-03 14:32:57 -0500

    class fixes?

---
+++ common/src/main/java/com/kyanite/deeperdarker/registry/blocks/custom/generic/CustomTrapdoorBlock.java
@@ -0,0 +1,10 @@
+package com.kyanite.deeperdarker.registry.blocks.custom.generic;
+
+import net.minecraft.world.level.block.TrapDoorBlock;
+import net.minecraft.world.level.block.state.properties.BlockSetType;
+
+public class CustomTrapdoorBlock extends TrapDoorBlock {
+    public CustomTrapdoorBlock(Properties properties, BlockSetType blockSetType) {
+        super(properties, blockSetType);
+    }
+}
