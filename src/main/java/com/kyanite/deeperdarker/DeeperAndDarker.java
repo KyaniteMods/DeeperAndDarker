68843016cf098923486e2d1eed93733358d65d53
nitrodynamite18 <69542632+nitrodynamite18@users.noreply.github.com>
2023-07-05 19:20:21 -0500

    Initial Fabric commit, still needs refactoring

---
+++ src/main/java/com/kyanite/deeperdarker/DeeperAndDarker.java
@@ -0,0 +1,16 @@
+package com.kyanite.deeperdarker;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DeeperAndDarker implements ModInitializer {
+	public static final String MOD_ID = "deeperdarker";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
