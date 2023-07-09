37752cae5438824c2e8c52259a29d4beed7dfd47
Pedro270707 <pedro2007.prrr@gmail.com>
2023-07-09 12:28:42 -0300

    Fixed door and trapdoor rendering

--- src/main/java/com/kyanite/deeperdarker/DeeperDarkerClient.java
+++ src/main/java/com/kyanite/deeperdarker/DeeperDarkerClient.java
@@ -1,10 +1,12 @@
 package com.kyanite.deeperdarker;
 
+import com.kyanite.deeperdarker.blocks.DeeperDarkerBlocks;
 import com.kyanite.deeperdarker.blocks.entity.DeeperDarkerBlockEntityTypes;
 import com.kyanite.deeperdarker.client.entity.feature.WardenHelmetHornsFeatureRenderer;
 import com.kyanite.deeperdarker.client.entity.model.DeeperDarkerModelLayers;
 import com.kyanite.deeperdarker.client.entity.model.WardenHelmetHornsModel;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.LivingEntityFeatureRendererRegistrationCallback;
 import net.fabricmc.fabric.mixin.lookup.BlockEntityTypeAccessor;
@@ -12,6 +14,7 @@ import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.model.Dilation;
 import net.minecraft.client.model.TexturedModelData;
+import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactories;
 import net.minecraft.client.render.block.entity.HangingSignBlockEntityRenderer;
 import net.minecraft.client.render.block.entity.SignBlockEntityRenderer;
@@ -21,6 +24,7 @@ public class DeeperDarkerClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+        BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutout(), DeeperDarkerBlocks.ECHO_DOOR, DeeperDarkerBlocks.ECHO_TRAPDOOR);
         BlockEntityRendererFactories.register(DeeperDarkerBlockEntityTypes.HANGING_SIGN, HangingSignBlockEntityRenderer::new);
         BlockEntityRendererFactories.register(DeeperDarkerBlockEntityTypes.SIGN, SignBlockEntityRenderer::new);
         EntityModelLayerRegistry.registerModelLayer(DeeperDarkerModelLayers.WARDEN_HELMET, WardenHelmetHornsModel::getTexturedModelData);
