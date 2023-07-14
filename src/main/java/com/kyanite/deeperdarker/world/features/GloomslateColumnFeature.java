49d096a86c40a5c3074ee47eba604abef52bb6c9
muhta <muhtasim.nabi@gmail.com>
2023-07-14 14:38:48 -0500

    shorter columns now have a taller inner base

--- src/main/java/com/kyanite/deeperdarker/world/features/GloomslateColumnFeature.java
+++ src/main/java/com/kyanite/deeperdarker/world/features/GloomslateColumnFeature.java
@@ -33,13 +33,13 @@ public class GloomslateColumnFeature extends Feature<NoneFeatureConfiguration> {
         pContext.level().setBlock(origin.below(), Blocks.RED_WOOL.defaultBlockState(), 3);
         pContext.level().setBlock(origin.above(columnHeight), Blocks.RED_WOOL.defaultBlockState(), 3);
 
-        boolean solid = columnHeight * 0.675f - columnHeight * 0.325f < 6;
+        boolean solid = columnHeight * 0.675f - columnHeight * 0.325f < 7;
         for(int i = 1; i < columnHeight + 1; i++) {
             int newY = origin.getY() + i - 1;
             float percentageToTop = i / ((float) columnHeight + 1);
             BlockPos pos = new BlockPos(origin.getX(), newY, origin.getZ());
 
-            if(percentageToTop >= 0.325f && percentageToTop <= 0.675f) { // if length of 5 or less, gloomslate
+            if(percentageToTop >= 0.325f && percentageToTop <= 0.675f) {
                 if(solid) pContext.level().setBlock(pos, DDBlocks.GLOOMSLATE.get().defaultBlockState(), 3);
                 else pContext.level().setBlock(pos, DDBlocks.CRYSTALLIZED_AMBER.get().defaultBlockState(), 3);
             } else if(!solid && percentageToTop >= 0.3f && percentageToTop <= 0.7f) {
@@ -49,8 +49,9 @@ public class GloomslateColumnFeature extends Feature<NoneFeatureConfiguration> {
             }
         }
 
-        columnBase(pContext.level(), pContext.random(), origin, columnHeight, true);
-        columnBase(pContext.level(), pContext.random(), origin.above(columnHeight - 1), columnHeight, false);
+        double multiplier = solid ? 1.2 : 1;
+        columnBase(pContext.level(), pContext.random(), origin, columnHeight, multiplier, true);
+        columnBase(pContext.level(), pContext.random(), origin.above(columnHeight - 1), columnHeight, multiplier, false);
 
         return true;
     }
@@ -63,29 +64,29 @@ public class GloomslateColumnFeature extends Feature<NoneFeatureConfiguration> {
         return false;
     }
 
-    private void columnBase(WorldGenLevel level, RandomSource random, BlockPos origin, int columnHeight, boolean bottom) {
+    private void columnBase(WorldGenLevel level, RandomSource random, BlockPos origin, int columnHeight, double multiplier, boolean bottom) {
         for(int i = 0; i < 4; i++) {
-            int baseHeight = random.nextInt((int) (0.36 * columnHeight), (int) (0.41 * columnHeight) + 1);
-            placeSection(level, random, origin, baseHeight, i, 1, columnHeight, bottom);
+            int baseHeight = random.nextInt((int) (0.36 * columnHeight * multiplier), (int) (0.41 * columnHeight * multiplier) + 1);
+            placeSection(level, random, origin, baseHeight, i, 1, multiplier, bottom);
         }
 
         for(int i = 0; i < 8; i++) {
             int baseHeight = random.nextInt((int) (0.22 * columnHeight), (int) (0.26 * columnHeight) + 1);
             if(i > 3) baseHeight *= 0.67;
-            placeSection(level, random, origin, baseHeight, i, 2, columnHeight, bottom);
+            placeSection(level, random, origin, baseHeight, i, 2, multiplier, bottom);
         }
 
-        if(columnHeight < 19) return;
+        if(multiplier > 1) return;
         for(int i = 0; i < 8; i++) {
             int baseHeight = random.nextInt((int) (0.04 * columnHeight), (int) (0.08 * columnHeight) + 1);
-            placeSection(level, random, origin, baseHeight, i, 3, columnHeight, bottom);
+            placeSection(level, random, origin, baseHeight, i, 3, multiplier, bottom);
         }
     }
 
-    private void placeSection(WorldGenLevel level, RandomSource random, BlockPos pos, int baseHeight, int iteration, int loop, int columnHeight, boolean bottom) {
+    private void placeSection(WorldGenLevel level, RandomSource random, BlockPos pos, int baseHeight, int iteration, int loop, double multiplier, boolean bottom) {
         float p = random.nextFloat();
         for(int j = 0; j < baseHeight; j++) {
-            if(iteration > 3 && columnHeight < 19) return;
+            if(iteration > 3 && multiplier > 1) return;
             if(j == baseHeight - 2 && j != 0  && p < 0.1f) level.setBlock(spread(bottom ? pos.above(j) : pos.below(j), iteration, loop), DDBlocks.GLOOMY_SCULK.get().defaultBlockState(), 3);
             else if(j == baseHeight - 1 && j != 0  && p < 0.22f) level.setBlock(spread(bottom ? pos.above(j) : pos.below(j), iteration, loop), DDBlocks.GLOOMY_SCULK.get().defaultBlockState(), 3);
             else level.setBlock(spread(bottom ? pos.above(j) : pos.below(j), iteration, loop), DDBlocks.GLOOMSLATE.get().defaultBlockState(), 3);
