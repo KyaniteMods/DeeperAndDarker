08e474a377c78918f931e46dbdbeb1e3c793da94
muhta <muhtasim.nabi@gmail.com>
2023-07-14 14:16:44 -0500

    replaced wool debugging (EXCEPT red wool) with gloomslate and gloomy sculk

--- src/main/java/com/kyanite/deeperdarker/world/features/GloomslateColumnFeature.java
+++ src/main/java/com/kyanite/deeperdarker/world/features/GloomslateColumnFeature.java
@@ -33,14 +33,16 @@ public class GloomslateColumnFeature extends Feature<NoneFeatureConfiguration> {
         pContext.level().setBlock(origin.below(), Blocks.RED_WOOL.defaultBlockState(), 3);
         pContext.level().setBlock(origin.above(columnHeight), Blocks.RED_WOOL.defaultBlockState(), 3);
 
+        boolean solid = columnHeight * 0.675f - columnHeight * 0.325f < 6;
         for(int i = 1; i < columnHeight + 1; i++) {
             int newY = origin.getY() + i - 1;
             float percentageToTop = i / ((float) columnHeight + 1);
             BlockPos pos = new BlockPos(origin.getX(), newY, origin.getZ());
 
-            if(percentageToTop >= 0.325f && percentageToTop <= 0.675f) {
-                pContext.level().setBlock(pos, DDBlocks.CRYSTALLIZED_AMBER.get().defaultBlockState(), 3);
-            } else if(percentageToTop >= 0.3f && percentageToTop <= 0.7f) {
+            if(percentageToTop >= 0.325f && percentageToTop <= 0.675f) { // if length of 5 or less, gloomslate
+                if(solid) pContext.level().setBlock(pos, DDBlocks.GLOOMSLATE.get().defaultBlockState(), 3);
+                else pContext.level().setBlock(pos, DDBlocks.CRYSTALLIZED_AMBER.get().defaultBlockState(), 3);
+            } else if(!solid && percentageToTop >= 0.3f && percentageToTop <= 0.7f) {
                 pContext.level().setBlock(pos, DDBlocks.GLOOMY_SCULK.get().defaultBlockState(), 3);
             } else {
                 pContext.level().setBlock(pos, DDBlocks.GLOOMSLATE.get().defaultBlockState(), 3);
@@ -64,33 +66,29 @@ public class GloomslateColumnFeature extends Feature<NoneFeatureConfiguration> {
     private void columnBase(WorldGenLevel level, RandomSource random, BlockPos origin, int columnHeight, boolean bottom) {
         for(int i = 0; i < 4; i++) {
             int baseHeight = random.nextInt((int) (0.36 * columnHeight), (int) (0.41 * columnHeight) + 1);
-            for(int j = 0; j < baseHeight; j++) {
-                if(j == baseHeight - 1 && random.nextFloat() < 0.25f) level.setBlock(spread(bottom ? origin.above(j) : origin.below(j), i, 1), Blocks.WHITE_WOOL.defaultBlockState(), 3);
-                else level.setBlock(spread(bottom ? origin.above(j) : origin.below(j), i, 1), Blocks.CYAN_WOOL.defaultBlockState(), 3);
-            }
+            placeSection(level, random, origin, baseHeight, i, 1, columnHeight, bottom);
         }
 
         for(int i = 0; i < 8; i++) {
             int baseHeight = random.nextInt((int) (0.22 * columnHeight), (int) (0.26 * columnHeight) + 1);
             if(i > 3) baseHeight *= 0.67;
-            for(int j = 0; j < baseHeight; j++) {
-                if(i < 4) {
-                    if(j == baseHeight - 1 && random.nextFloat() < 0.25f) level.setBlock(spread(bottom ? origin.above(j) : origin.below(j), i, 2), Blocks.WHITE_WOOL.defaultBlockState(), 3);
-                    else level.setBlock(spread(bottom ? origin.above(j) : origin.below(j), i, 2), Blocks.PURPLE_WOOL.defaultBlockState(), 3);
-                } else if(columnHeight > 18) {
-                    if(j == baseHeight - 1 && random.nextFloat() < 0.25f) level.setBlock(spread(bottom ? origin.above(j) : origin.below(j), i, 2), Blocks.WHITE_WOOL.defaultBlockState(), 3);
-                    else level.setBlock(spread(bottom ? origin.above(j) : origin.below(j), i, 2), Blocks.PINK_WOOL.defaultBlockState(), 3);
-                }
-            }
+            placeSection(level, random, origin, baseHeight, i, 2, columnHeight, bottom);
         }
 
         if(columnHeight < 19) return;
         for(int i = 0; i < 8; i++) {
             int baseHeight = random.nextInt((int) (0.04 * columnHeight), (int) (0.08 * columnHeight) + 1);
-            for(int j = 0; j < baseHeight; j++) {
-                if(j == baseHeight - 1 && random.nextFloat() < 0.25f) level.setBlock(spread(bottom ? origin.above(j) : origin.below(j), i, 3), Blocks.WHITE_WOOL.defaultBlockState(), 3);
-                else level.setBlock(spread(bottom ? origin.above(j) : origin.below(j), i, 3), Blocks.LIME_WOOL.defaultBlockState(), 3);
-            }
+            placeSection(level, random, origin, baseHeight, i, 3, columnHeight, bottom);
+        }
+    }
+
+    private void placeSection(WorldGenLevel level, RandomSource random, BlockPos pos, int baseHeight, int iteration, int loop, int columnHeight, boolean bottom) {
+        float p = random.nextFloat();
+        for(int j = 0; j < baseHeight; j++) {
+            if(iteration > 3 && columnHeight < 19) return;
+            if(j == baseHeight - 2 && j != 0  && p < 0.1f) level.setBlock(spread(bottom ? pos.above(j) : pos.below(j), iteration, loop), DDBlocks.GLOOMY_SCULK.get().defaultBlockState(), 3);
+            else if(j == baseHeight - 1 && j != 0  && p < 0.22f) level.setBlock(spread(bottom ? pos.above(j) : pos.below(j), iteration, loop), DDBlocks.GLOOMY_SCULK.get().defaultBlockState(), 3);
+            else level.setBlock(spread(bottom ? pos.above(j) : pos.below(j), iteration, loop), DDBlocks.GLOOMSLATE.get().defaultBlockState(), 3);
         }
     }
 
