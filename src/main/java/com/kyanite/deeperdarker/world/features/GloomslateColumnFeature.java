5f0112bd90647ebec5f49cb484082f855e0af24b
muhta <muhtasim.nabi@gmail.com>
2023-07-14 03:19:53 -0500

    in the process of making gloomslate column better

--- src/main/java/com/kyanite/deeperdarker/world/features/GloomslateColumnFeature.java
+++ src/main/java/com/kyanite/deeperdarker/world/features/GloomslateColumnFeature.java
@@ -3,6 +3,7 @@ package com.kyanite.deeperdarker.world.features;
 import com.kyanite.deeperdarker.content.DDBlocks;
 import com.mojang.serialization.Codec;
 import net.minecraft.core.BlockPos;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.WorldGenLevel;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
@@ -17,46 +18,95 @@ public class GloomslateColumnFeature extends Feature<NoneFeatureConfiguration> {
 
     @Override
     public boolean place(FeaturePlaceContext<NoneFeatureConfiguration> pContext) {
-        int y = pContext.origin().getY();
-        int pillarHeight = getHeight(pContext);
+        BlockPos origin = pContext.origin();
+        int columnHeight = 0;
+        while(true) {
+            BlockPos pos = new BlockPos(origin.getX(), origin.getY() + columnHeight + 1, origin.getZ());
+            if(pContext.level().getBlockState(pos.below()).isAir()) columnHeight++;
+            else break;
+        }
 
-        BlockState state = pContext.level().getBlockState(pContext.origin().below());
+        BlockState state = pContext.level().getBlockState(origin.below());
 
-        if(noSpace(pContext.level(), pContext.origin(), pillarHeight)) return false;
+        if(anyObstruction(pContext.level(), origin, columnHeight)) return false;
         if(!state.is(DDBlocks.GLOOMY_SCULK.get())) return false;
-        pContext.level().setBlock(pContext.origin().below(), DDBlocks.GLOOMSLATE.get().defaultBlockState(), 3);
+        pContext.level().setBlock(origin.below(), Blocks.RED_WOOL.defaultBlockState(), 3);
 
-        for(int i = 1; i < pillarHeight + 1; i++) {
-            int newY = y + i - 1;
-            float percentageToTop = i / ((float) pillarHeight + 1);
-            BlockPos pos = new BlockPos(pContext.origin().getX(), newY, pContext.origin().getZ());
+        for(int i = 1; i < columnHeight + 1; i++) {
+            int newY = origin.getY() + i - 1;
+            float percentageToTop = i / ((float) columnHeight + 1);
+            BlockPos pos = new BlockPos(origin.getX(), newY, origin.getZ());
 
-            if(percentageToTop >= 0.45f && percentageToTop <= 0.7f) {
+            if(percentageToTop >= 0.45f && percentageToTop <= 0.6f) {
                 pContext.level().setBlock(pos, DDBlocks.CRYSTALLIZED_AMBER.get().defaultBlockState(), 3);
-            } else if(percentageToTop >= 0.39f && percentageToTop <= 0.76f) {
+            } else if(percentageToTop >= 0.39f && percentageToTop <= 0.66f) {
                 pContext.level().setBlock(pos, DDBlocks.GLOOMY_SCULK.get().defaultBlockState(), 3);
             } else {
                 pContext.level().setBlock(pos, DDBlocks.GLOOMSLATE.get().defaultBlockState(), 3);
             }
         }
 
+        base(pContext.level(), pContext.random(), origin, columnHeight);
+
         return true;
     }
 
-    private int getHeight(FeaturePlaceContext<NoneFeatureConfiguration> context) {
-        int height = 0;
-        while (true) {
-            BlockPos pos = new BlockPos(context.origin().getX(), context.origin().getY() + height + 1, context.origin().getZ());
-            if(context.level().getBlockState(pos.below()).isAir()) height = height + 1;
-            else return height;
-        }
-    }
-
-    private boolean noSpace(WorldGenLevel level, BlockPos pos, int distance) {
+    private boolean anyObstruction(WorldGenLevel level, BlockPos pos, int distance) {
         for(int i = 0; i < distance; i++) {
             if(!level.getBlockState(pos.above(i)).is(Blocks.AIR)) return true;
         }
 
         return false;
+    }
+
+    private void base(WorldGenLevel level, RandomSource random, BlockPos origin, int columnHeight) {
+        for(int i = 0; i < 4; i++) {
+            int baseHeight = random.nextInt((int) (0.36 * columnHeight), (int) (0.4 * columnHeight) + 1);
+            for(int j = 0; j < baseHeight; j++) {
+                level.setBlock(direction(origin.above(j), i, 1), Blocks.CYAN_WOOL.defaultBlockState(), 3);
+            }
+        }
+
+        for(int i = 0; i < 4; i++) {
+            int baseHeight = random.nextInt((int) (0.26 * columnHeight), (int) (0.3 * columnHeight) + 1);
+            for(int j = 0; j < baseHeight; j++) {
+                level.setBlock(direction(origin.above(j), i, 2), Blocks.PURPLE_WOOL.defaultBlockState(), 3);
+            }
+        }
+
+        for(int i = 0; i < 8; i++) {
+            int baseHeight = random.nextInt((int) (0.15 * columnHeight), (int) (0.22 * columnHeight) + 1);
+            for(int j = 0; j < baseHeight; j++) {
+                level.setBlock(direction(origin.above(j), i, 3), Blocks.LIME_WOOL.defaultBlockState(), 3);
+            }
+        }
+    }
+
+    private BlockPos direction(BlockPos pos, int index, int loop) {
+        BlockPos basePos = pos;
+        for(int i = 0; i < loop; i++) {
+            int j = i % 2;
+            if(index > 3 && i != 1) j += 2;
+            switch ((index + j) % 4) {
+                default -> basePos = basePos.north();
+                case 1 -> basePos = basePos.east();
+                case 2 -> basePos = basePos.south();
+                case 3 -> basePos = basePos.west();
+            }
+            // debug for loop = 3
+            //     i =    0  1  2
+            //     j =    0  1  0
+            // index = 0: n, e, n
+            // index = 1: e, s, e
+            // index = 2: s, w, s
+            // index = 3: w, n, w
+            //     j =    2  3  2
+            // index = 4: s, e, s
+            // index = 5: w, s, w
+            // index = 6: n, w, n
+            // index = 7: e, n, e
+        }
+
+        return basePos;
     }
 }
