d52dd82c53084048b5a2ce1cce839615a74f0058
muhta <muhtasim.nabi@gmail.com>
2023-07-15 00:52:37 -0500

    changed sculk affinity effect color

--- src/main/java/com/kyanite/deeperdarker/content/DDEffects.java
+++ src/main/java/com/kyanite/deeperdarker/content/DDEffects.java
@@ -9,7 +9,7 @@ import net.minecraftforge.registries.RegistryObject;
 
 public class DDEffects {
     public static final DeferredRegister<MobEffect> EFFECTS = DeferredRegister.create(ForgeRegistries.MOB_EFFECTS, DeeperDarker.MOD_ID);
-    public static final RegistryObject<MobEffect> SCULK_AFFINITY = EFFECTS.register("sculk_affinity", () -> new MobEffect(MobEffectCategory.BENEFICIAL, 0x66dbff) {
+    public static final RegistryObject<MobEffect> SCULK_AFFINITY = EFFECTS.register("sculk_affinity", () -> new MobEffect(MobEffectCategory.BENEFICIAL, 0x00ffd0) {
         @Override
         public boolean isDurationEffectTick(int pDuration, int pAmplifier) {
             return true;
