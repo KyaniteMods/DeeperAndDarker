73156a0fc10c6d8d7d72b403142f0428dfbf6e04
muhta <muhtasim.nabi@gmail.com>
2023-07-15 01:58:01 -0500

    warden carapace now drops from the warden and rarely spawns in ancient city chests

--- src/main/java/com/kyanite/deeperdarker/datagen/data/DDLootModifierProvider.java
+++ src/main/java/com/kyanite/deeperdarker/datagen/data/DDLootModifierProvider.java
@@ -6,6 +6,7 @@ import com.kyanite.deeperdarker.content.loot.AddItemModifier;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
+import net.minecraft.world.level.storage.loot.predicates.LootItemRandomChanceCondition;
 import net.minecraftforge.common.data.GlobalLootModifierProvider;
 import net.minecraftforge.common.loot.LootTableIdCondition;
 
@@ -17,5 +18,7 @@ public class DDLootModifierProvider extends GlobalLootModifierProvider {
     @Override
     protected void start() {
         add("warden_upgrade_from_ancient_city", new AddItemModifier(new LootItemCondition[]{ new LootTableIdCondition.Builder(new ResourceLocation("chests/ancient_city")).build() }, DDItems.WARDEN_UPGRADE_SMITHING_TEMPLATE.get()));
+        add("warden_carapace_from_ancient_city", new AddItemModifier(new LootItemCondition[]{ new LootTableIdCondition.Builder(new ResourceLocation("chests/ancient_city")).build(), LootItemRandomChanceCondition.randomChance(0.2f).build() }, DDItems.WARDEN_CARAPACE.get()));
+        add("warden_carapace_from_warden", new AddItemModifier(new LootItemCondition[]{ new LootTableIdCondition.Builder(new ResourceLocation("entities/warden")).build() }, DDItems.WARDEN_CARAPACE.get()));
     }
 }
