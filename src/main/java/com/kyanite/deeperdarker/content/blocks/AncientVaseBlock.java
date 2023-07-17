f26ee643e5562b7435b858ba0644d7f064024c9e
muhta <muhtasim.nabi@gmail.com>
2023-07-17 18:37:16 -0500

    the ancient vase now has a 12.5% chance to spawn leeches when broken

--- src/main/java/com/kyanite/deeperdarker/content/blocks/AncientVaseBlock.java
+++ src/main/java/com/kyanite/deeperdarker/content/blocks/AncientVaseBlock.java
@@ -1,9 +1,13 @@
 package com.kyanite.deeperdarker.content.blocks;
 
+import com.kyanite.deeperdarker.content.DDEntities;
+import com.kyanite.deeperdarker.content.entities.SculkLeech;
 import net.minecraft.core.BlockPos;
+import net.minecraft.util.RandomSource;
 import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.SimpleWaterloggedBlock;
@@ -50,5 +54,20 @@ public class AncientVaseBlock extends DropExperienceBlock implements SimpleWater
     @Override
     public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
         return Stream.of(BASE, OUTLINE, RIM).reduce((v1, v2) -> Shapes.join(v1, v2, BooleanOp.OR)).get();
+    }
+
+    @Override
+    public void onRemove(BlockState pState, Level pLevel, BlockPos pPos, BlockState pNewState, boolean pMovedByPiston) {
+        super.onRemove(pState, pLevel, pPos, pNewState, pMovedByPiston);
+
+        RandomSource random = RandomSource.create();
+        if(random.nextFloat() < 0.125f) {
+            for(int i = 0; i < random.nextInt(1, 4); i++) {
+                SculkLeech entity = DDEntities.SCULK_LEECH.get().create(pLevel);
+                assert entity != null;
+                entity.moveTo(pPos.getX() + random.nextFloat(), pPos.getY() + random.nextFloat() + 0.15f, pPos.getZ() + random.nextFloat(), random.nextFloat() * 360, random.nextFloat() * 360);
+                pLevel.addFreshEntity(entity);
+            }
+        }
     }
 }
