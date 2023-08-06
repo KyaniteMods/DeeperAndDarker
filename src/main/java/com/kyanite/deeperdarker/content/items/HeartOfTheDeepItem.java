aa046d570c534e058667d882137e5cbf72ae0ad4
Pedro270707 <pedro2007.prrr@gmail.com>
2023-08-06 16:14:42 -0300

    Heart of the deep now beats

---
+++ src/main/java/com/kyanite/deeperdarker/content/items/HeartOfTheDeepItem.java
@@ -0,0 +1,21 @@
+package com.kyanite.deeperdarker.content.items;
+
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+
+public class HeartOfTheDeepItem extends Item {
+    public HeartOfTheDeepItem(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public void inventoryTick(ItemStack pItemStack, Level pLevel, Entity pEntity, int i, boolean bl) {
+        if(RandomSource.create().nextFloat() < 0.012f) {
+            pEntity.playSound(SoundEvents.WARDEN_HEARTBEAT, 1.7f, 1f);
+        }
+    }
+}
