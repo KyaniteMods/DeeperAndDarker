ae553421b4feb063c70eb1547e4b1001d8191bef
Pedro <pedro2007.prrr@gmail.com>
2023-10-31 16:54:15 -0300

    Fixed player spawning in the void

--- src/main/java/com/kyanite/deeperdarker/DeeperDarker.java
+++ src/main/java/com/kyanite/deeperdarker/DeeperDarker.java
@@ -56,6 +56,7 @@ public class DeeperDarker implements ModInitializer {
 				.customPortalBlock((CustomPortalBlock) DDBlocks.OTHERSIDE_PORTAL)
 				.forcedSize(8, 4)
 				.registerInPortalAmbienceSound((player) -> new CPASoundEventData(DDSounds.PORTAL_GROAN, 1.0f, 1.0f))
+				.setPortalSearchYRange(0, 128)
 				.registerPortal();
 
 		LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
