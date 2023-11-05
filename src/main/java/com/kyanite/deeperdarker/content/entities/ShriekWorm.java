41ff0bde76a9a78a6749461f95c850dacbbcdf51
muhta <muhtasim.nabi@gmail.com>
2023-11-04 19:54:51 -0500

    shriek worm fixes

--- src/main/java/com/kyanite/deeperdarker/content/entities/ShriekWorm.java
+++ src/main/java/com/kyanite/deeperdarker/content/entities/ShriekWorm.java
@@ -5,8 +5,6 @@ import net.minecraft.core.particles.BlockParticleOption;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.syncher.EntityDataAccessor;
-import net.minecraft.network.syncher.EntityDataSerializers;
-import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.damagesource.DamageSource;
@@ -25,13 +23,14 @@ import org.jetbrains.annotations.Nullable;
 
 @SuppressWarnings("deprecation, NullableProblems")
 public class ShriekWorm extends Monster {
-    private static final EntityDataAccessor<Integer> IDLE_TIMER = SynchedEntityData.defineId(ShriekWorm.class, EntityDataSerializers.INT);
     public final AnimationState idleState = new AnimationState();
     public final AnimationState attackState = new AnimationState();
     public final AnimationState asleepState = new AnimationState();
     public final AnimationState emergeState = new AnimationState();
     public final AnimationState descendState = new AnimationState();
-    private int emerging;
+    private int emergingTime;
+    private int idleTime;
+    private boolean asleep;
 
     public ShriekWorm(EntityType<? extends Monster> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
@@ -73,12 +72,6 @@ public class ShriekWorm extends Monster {
     }
 
     @Override
-    protected void defineSynchedData() {
-        super.defineSynchedData();
-        this.entityData.define(IDLE_TIMER, getRandom().nextInt(160, 300));
-    }
-
-    @Override
     public boolean doHurtTarget(Entity pEntity) {
         this.level().broadcastEntityEvent(this, (byte) 4);
         return super.doHurtTarget(pEntity);
@@ -88,40 +81,41 @@ public class ShriekWorm extends Monster {
     public void tick() {
         super.tick();
 
-        if(this.getPose() == Pose.EMERGING && ++emerging > 156) this.setPose(Pose.STANDING);
+        if(this.getPose() == Pose.EMERGING && ++emergingTime > 80) this.setPose(Pose.STANDING);
+
+        if(this.getPose() == Pose.STANDING && !this.asleep) {
+            this.idleTime++;
+            if(this.idleTime > 200) {
+                this.idleTime = 0;
+                this.asleep = true;
+            }
+        }
+
+        Player player = level().getNearestPlayer(this, 5);
+        if(player != null && !player.isDeadOrDying() && !player.isCreative()) {
+            this.asleep = false;
+        } else {
+            if(this.attackState.isStarted() && !this.idleState.isStarted()) {
+                this.attackState.stop();
+                this.idleState.start(this.tickCount);
+            }
+        }
 
         if(level().isClientSide()) {
-            if(this.idleState.isStarted()) {
-                this.entityData.set(IDLE_TIMER, this.entityData.get(IDLE_TIMER) - 1);
-
-                if(this.entityData.get(IDLE_TIMER) <= 0) {
-                    this.idleState.stop();
-                    if(this.random.nextFloat() < 1f) {
-                        this.asleepState.start(this.tickCount);
-                    }
-                    else this.descendState.start(this.tickCount); // chance for descend is 0 bc kill function is broken
-                }
-            } else if(!this.asleepState.isStarted() && !this.emergeState.isStarted() && !this.descendState.isStarted()) {
-                if(!this.attackState.isStarted()) {
-                    this.idleState.start(this.tickCount);
-                } else if(this.entityData.get(IDLE_TIMER) < 160) {
-                    this.entityData.set(IDLE_TIMER, getRandom().nextInt(160, 300));
-                }
+            if(this.asleep && !this.asleepState.isStarted()) {
+                this.idleState.stop();
+                this.asleepState.start(this.tickCount);
+            }
+            if(!this.asleep && !this.idleState.isStarted() && !this.emergeState.isStarted() && !this.descendState.isStarted()) {
+                this.asleepState.stop();
+                this.idleState.start(this.tickCount);
             }
 
             if(this.getPose() == Pose.EMERGING) {
                 double sX = this.random.nextGaussian() * 0.02;
                 double sY = this.random.nextGaussian() * 0.02;
                 double sZ = this.random.nextGaussian() * 0.02;
-                level().addParticle(new BlockParticleOption(ParticleTypes.BLOCK, this.getBlockStateOn()), this.getX() - this.random.nextDouble(), this.getY() + 1, this.getZ() - this.random.nextDouble(), sX, sY, sZ);
-            }
-        }
-
-        Player player = level().getNearestPlayer(this, 3);
-        if(player == null || player.isDeadOrDying() || player.isCreative()) {
-            if(this.attackState.isStarted()) {
-                this.attackState.stop();
-                this.idleState.start(this.tickCount);
+                level().addParticle(new BlockParticleOption(ParticleTypes.BLOCK, this.getBlockStateOn()), getRandomX(1), getY() + 1, getRandomZ(1), sX, sY, sZ);
             }
         }
 
