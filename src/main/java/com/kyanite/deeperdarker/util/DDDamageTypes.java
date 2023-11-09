63016658c652ad326808536bf3223f54e9abfb88
muhta <muhtasim.nabi@gmail.com>
2023-11-09 17:09:34 -0600

    changed damage scaling for jaw damage

--- src/main/java/com/kyanite/deeperdarker/util/DDDamageTypes.java
+++ src/main/java/com/kyanite/deeperdarker/util/DDDamageTypes.java
@@ -5,7 +5,6 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstapContext;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.damagesource.DamageScaling;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.damagesource.DamageType;
 import net.minecraft.world.entity.Entity;
@@ -16,7 +15,7 @@ public class DDDamageTypes {
     public static final ResourceKey<DamageType> RING = createKey("ring");
 
     public static void bootstrap(BootstapContext<DamageType> context) {
-        context.register(JAW, new DamageType("jaw", DamageScaling.ALWAYS, 0.1f));
+        context.register(JAW, new DamageType("jaw", 0.1f));
         context.register(RING, new DamageType("ring", 0.1f));
     }
 
