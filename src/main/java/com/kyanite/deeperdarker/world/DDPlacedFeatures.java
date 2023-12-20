4cd0a93ad332e57419752efc1f44e2aaa51f2e4f
NewJumper <muhtasim.nabi@gmail.com>
2023-12-20 00:24:44 -0600

    made pools more common in caverns

--- src/main/java/com/kyanite/deeperdarker/world/DDPlacedFeatures.java
+++ src/main/java/com/kyanite/deeperdarker/world/DDPlacedFeatures.java
@@ -100,7 +100,7 @@ public class DDPlacedFeatures {
         PlacementUtils.register(context, SCULK_DIAMOND, features.getOrThrow(DDConfiguredFeatures.ORE_SCULK_DIAMOND), countPlacement(3, HeightRangePlacement.triangle(VerticalAnchor.aboveBottom(-32), VerticalAnchor.aboveBottom(50))));
 
         PlacementUtils.register(context, BLOOMING_MOSS, features.getOrThrow(DDConfiguredFeatures.ORE_BLOOMING_MOSS), countPlacement(19, PlacementUtils.FULL_RANGE));
-        PlacementUtils.register(context, BLOOMING_POOLS, features.getOrThrow(DDConfiguredFeatures.BLOOMING_POOL), CountPlacement.of(10), InSquarePlacement.spread(), PlacementUtils.FULL_RANGE, EnvironmentScanPlacement.scanningFor(Direction.DOWN, BlockPredicate.solid(), BlockPredicate.ONLY_IN_AIR_PREDICATE, 12), RandomOffsetPlacement.vertical(ConstantInt.of(1)), BiomeFilter.biome());
+        PlacementUtils.register(context, BLOOMING_POOLS, features.getOrThrow(DDConfiguredFeatures.BLOOMING_POOL), CountPlacement.of(16), InSquarePlacement.spread(), PlacementUtils.FULL_RANGE, EnvironmentScanPlacement.scanningFor(Direction.DOWN, BlockPredicate.solid(), BlockPredicate.ONLY_IN_AIR_PREDICATE, 12), RandomOffsetPlacement.vertical(ConstantInt.of(1)), BiomeFilter.biome());
 
         PlacementUtils.register(context, GLOOMY_SCULK, features.getOrThrow(DDConfiguredFeatures.ORE_GLOOMY_SCULK), countPlacement(96, HeightRangePlacement.uniform(VerticalAnchor.bottom(), VerticalAnchor.aboveBottom(32))));
         PlacementUtils.register(context, MAGMA, features.getOrThrow(DDConfiguredFeatures.ORE_MAGMA), countPlacement(128, PlacementUtils.FULL_RANGE));
