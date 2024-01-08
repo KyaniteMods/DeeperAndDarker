ea2ba534e2a5f82b12c4a59bbceea2a2277c5b98
Pedro <pedro2007.prrr@gmail.com>
2024-01-08 00:30:15 -0300

    Added tip to soul elytra

--- src/main/java/com/kyanite/deeperdarker/util/datagen/DDENLanguageProvider.java
+++ src/main/java/com/kyanite/deeperdarker/util/datagen/DDENLanguageProvider.java
@@ -166,6 +166,7 @@ public class DDENLanguageProvider extends FabricLanguageProvider {
         translationBuilder.add(DDItems.STALKER_SPAWN_EGG, "Stalker Spawn Egg");
         translationBuilder.add(DDItems.SCULK_CENTIPEDE_SPAWN_EGG, "Sculk Centipede Spawn Egg");
         translationBuilder.add(DDItems.SOUL_ELYTRA, "Soul Elytra");
+        translationBuilder.add(DDItems.SOUL_ELYTRA.getDescriptionId() + ".boost", "Press %s to Boost");
 
         translationBuilder.add(DDEnchantments.CATALYSIS, "Catalysis");
         translationBuilder.add(DDEnchantments.CATALYSIS.getDescriptionId() + ".desc", "Spreads sculk when mobs are killed.");
