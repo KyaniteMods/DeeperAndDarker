029f9285d124b80dce9f49d25bf70f60ead20a07
NewJumper <muhtasim.nabi@gmail.com>
2024-04-02 19:00:45 -0300

    clean up

--- src/main/java/com/kyanite/deeperdarker/mixin/VibrationListenerMixin.java
+++ src/main/java/com/kyanite/deeperdarker/mixin/VibrationListenerMixin.java
@@ -18,7 +18,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(VibrationSystem.Listener.class)
 public class VibrationListenerMixin {
     @Inject(method = "handleGameEvent", at = @At("HEAD"), cancellable = true)
-    public void handleGameEvent(ServerLevel level, GameEvent event, GameEvent.Context context, Vec3 vec3, CallbackInfoReturnable<? super Boolean> cir) {
+    public void handleGameEvent(ServerLevel level, GameEvent event, GameEvent.Context context, Vec3 vec3, CallbackInfoReturnable<Boolean> cir) {
         if(context.sourceEntity() instanceof LivingEntity entity) {
             if((event.equals(GameEvent.STEP) || event.equals(GameEvent.HIT_GROUND) || event.equals(GameEvent.SWIM) || event.equals(GameEvent.SPLASH)) && hasArmorThatDampensVibrations(entity)) cir.setReturnValue(false);
             if(entity.hasEffect(DDEffects.SCULK_AFFINITY)) cir.setReturnValue(false);
