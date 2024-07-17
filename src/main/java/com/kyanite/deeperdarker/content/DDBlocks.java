8ce1629cb58c333c5eb095d6bf801614687d121b
NewJumper <muhtasim.nabi@gmail.com>
2024-07-17 17:12:29 -0500

    fixed echo saplings

--- src/main/java/com/kyanite/deeperdarker/content/DDBlocks.java
+++ src/main/java/com/kyanite/deeperdarker/content/DDBlocks.java
@@ -6,6 +6,7 @@ import com.kyanite.deeperdarker.content.blocks.entity.DDHangingSignBlockEntity;
 import com.kyanite.deeperdarker.content.blocks.entity.DDSignBlockEntity;
 import com.kyanite.deeperdarker.content.blocks.flammable.*;
 import com.kyanite.deeperdarker.content.blocks.vegetation.*;
+import com.kyanite.deeperdarker.world.DDConfiguredFeatures;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.util.valueproviders.ConstantInt;
@@ -25,6 +26,7 @@ import net.minecraft.world.level.material.PushReaction;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
+import java.util.Optional;
 import java.util.function.Supplier;
 
 @SuppressWarnings("NullableProblems")
@@ -33,7 +35,6 @@ public class DDBlocks {
 
     private static final BlockSetType ECHO_SET = BlockSetType.register(new BlockSetType("echo"));
     public static final WoodType ECHO = WoodType.register(new WoodType("echo", ECHO_SET));
-//    public static final TreeGrower ECHO_TREE = new TreeGrower("echo", Optional.empty(), Optional.of(DDConfiguredFeatures.TREE_ECHO), Optional.empty());
 
     public static final DeferredBlock<RotatedPillarBlock> ECHO_LOG = register("echo_log", () -> new RotatedFlammableBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_LOG).mapColor(state -> state.getValue(RotatedPillarBlock.AXIS) == Direction.Axis.Y ? MapColor.COLOR_LIGHT_GRAY : MapColor.COLOR_PURPLE), 5, 5));
     public static final DeferredBlock<RotatedPillarBlock> ECHO_WOOD = register("echo_wood", () -> new RotatedFlammableBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_WOOD).mapColor(MapColor.COLOR_PURPLE), 5, 5));
@@ -49,7 +50,8 @@ public class DDBlocks {
     public static final DeferredBlock<PressurePlateBlock> ECHO_PRESSURE_PLATE = register("echo_pressure_plate", () -> new PressurePlateBlock(ECHO_SET, BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PRESSURE_PLATE).mapColor(MapColor.COLOR_LIGHT_GRAY)));
     public static final DeferredBlock<ButtonBlock> ECHO_BUTTON = register("echo_button", () -> new ButtonBlock(ECHO_SET, 30, BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_BUTTON)));
     public static final DeferredBlock<LeavesBlock> ECHO_LEAVES = register("echo_leaves", () -> new FlammableLeavesBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_LEAVES).mapColor(MapColor.COLOR_PURPLE), 60, 30));
-    public static final DeferredBlock<SaplingBlock> ECHO_SAPLING = register("echo_sapling", () -> new SaplingBlock(TreeGrower.OAK, BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_SAPLING)) {
+    public static final TreeGrower ECHO_TREE = new TreeGrower("echo", Optional.empty(), Optional.of(DDConfiguredFeatures.TREE_ECHO), Optional.empty());
+    public static final DeferredBlock<SaplingBlock> ECHO_SAPLING = register("echo_sapling", () -> new SaplingBlock(ECHO_TREE, BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_SAPLING)) {
         @Override
         protected boolean mayPlaceOn(BlockState pState, BlockGetter pLevel, BlockPos pPos) {
             return pState.is(DDBlocks.ECHO_SOIL.get()) || pState.is(DDBlocks.SCULK_GRIME.get());
