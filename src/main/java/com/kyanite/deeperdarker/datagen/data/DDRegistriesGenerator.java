c5f230833b81f9233fd439c47d184ab2daa7c6c8
NewJumper <muhtasim.nabi@gmail.com>
2024-07-16 23:19:20 -0500

    made registries datagen easier to read

--- src/main/java/com/kyanite/deeperdarker/datagen/data/DDRegistriesGenerator.java
+++ src/main/java/com/kyanite/deeperdarker/datagen/data/DDRegistriesGenerator.java
@@ -20,7 +20,16 @@ import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
 public class DDRegistriesGenerator extends DatapackBuiltinEntriesProvider {
-    public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder().add(Registries.BIOME, OthersideBiomes::bootstrap).add(Registries.CONFIGURED_FEATURE, DDConfiguredFeatures::bootstrap).add(Registries.DAMAGE_TYPE, DDDamageTypes::bootstrap).add(Registries.DIMENSION_TYPE, OthersideDimension::bootstrap).add(Registries.PLACED_FEATURE, DDPlacedFeatures::bootstrap).add(Registries.PROCESSOR_LIST, DDProcessorLists::bootstrap).add(Registries.STRUCTURE, DDStructures::bootstrap).add(Registries.STRUCTURE_SET, DDStructureSets::bootstrap).add(Registries.TEMPLATE_POOL, DDPools::bootstrap);
+    public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
+            .add(Registries.BIOME, OthersideBiomes::bootstrap)
+            .add(Registries.CONFIGURED_FEATURE, DDConfiguredFeatures::bootstrap)
+            .add(Registries.DAMAGE_TYPE, DDDamageTypes::bootstrap)
+            .add(Registries.DIMENSION_TYPE, OthersideDimension::bootstrap)
+            .add(Registries.PLACED_FEATURE, DDPlacedFeatures::bootstrap)
+            .add(Registries.PROCESSOR_LIST, DDProcessorLists::bootstrap)
+            .add(Registries.STRUCTURE, DDStructures::bootstrap)
+            .add(Registries.STRUCTURE_SET, DDStructureSets::bootstrap)
+            .add(Registries.TEMPLATE_POOL, DDPools::bootstrap);
 
     public DDRegistriesGenerator(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
         super(output, registries, BUILDER, Set.of(DeeperDarker.MOD_ID));
