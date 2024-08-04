e7ea876e4059059e406ad28ff18cd608c4d7dab6
Pedro <pedro2007.prrr@gmail.com>
2024-08-04 16:18:05 -0300

    Fixed issues outside of IDE

--- src/main/java/com/kyanite/deeperdarker/DeeperDarkerEarlyRisers.java
+++ src/main/java/com/kyanite/deeperdarker/DeeperDarkerEarlyRisers.java
@@ -1,13 +1,11 @@
 package com.kyanite.deeperdarker;
 
 import com.chocohead.mm.api.ClassTinkerers;
-import com.kyanite.deeperdarker.content.DDSounds;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.MappingResolver;
 import net.minecraft.core.Holder;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
-import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
 
 import java.lang.reflect.Field;
 
@@ -15,13 +13,12 @@ public class DeeperDarkerEarlyRisers implements Runnable {
     @Override
     public void run() {
         MappingResolver remapper = FabricLoader.getInstance().getMappingResolver();
-
         String instrument = remapper.mapClassName("intermediary", "net.minecraft.class_2766");
         String type = remapper.mapClassName("intermediary", "net.minecraft.class_2766$class_7994");
         String holder = remapper.mapClassName("intermediary", "net.minecraft.class_6880");
-        String mobHead = remapper.mapFieldName("intermediary", "net.minecraft.class_2766$class_7994", "field_41607", "Lnet/minecraft/world/level/block/state/properties/NoteBlockInstrument$Type;");
+        String mobHead = remapper.mapFieldName("intermediary", "net.minecraft.class_2766$class_7994", "field_41607", "Lnet/minecraft/class_2766$class_7994;");
         try {
-            Field field = Class.forName(type).getDeclaredField("MOB_HEAD");
+            Field field = Class.forName(type).getDeclaredField(mobHead);
             field.setAccessible(true);
             Object obj = field.get(null);
             ClassTinkerers.enumBuilder(instrument, "Ljava/lang/String;", "L" + holder + ";", "L" + type + ";").addEnum("deeperdarker$SHATTERED", "deeperdarker_shattered", Holder.direct(SoundEvent.createVariableRangeEvent(new ResourceLocation(DeeperDarker.MOD_ID, "block.note_block.imitate.shattered"))), obj).build();
