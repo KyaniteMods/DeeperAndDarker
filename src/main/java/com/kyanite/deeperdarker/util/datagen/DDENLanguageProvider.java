d310936bcb7e2aefa4ca782e09cd0e4d9617a2df
Pedro <pedro2007.prrr@gmail.com>
2024-08-03 21:26:06 -0300

    Flowerless ice lilies now show that in their tooltips

--- src/main/java/com/kyanite/deeperdarker/util/datagen/DDENLanguageProvider.java
+++ src/main/java/com/kyanite/deeperdarker/util/datagen/DDENLanguageProvider.java
@@ -171,7 +171,7 @@ public class DDENLanguageProvider extends FabricLanguageProvider {
         translationBuilder.add(DDBlocks.SOUNDPROOF_GLASS, "Soundproof Glass");
         translationBuilder.add(DDBlocks.OTHERSIDE_PORTAL, "Otherside Portal");
         translationBuilder.add(DDBlocks.ICE_LILY, "Ice Lily");
-        translationBuilder.add(Util.makeDescriptionId("block", BuiltInRegistries.BLOCK.getKey(DDBlocks.ICE_LILY).withPrefix("flowerless_")), "Flowerless Ice Lily");
+        translationBuilder.add(Util.makeDescriptionId("tooltips", BuiltInRegistries.BLOCK.getKey(DDBlocks.ICE_LILY)) + ".flowerless", "Flowerless");
         translationBuilder.add(DDBlocks.LILY_FLOWER, "Lily Flower");
         translationBuilder.add(DDItems.LILY_FLOWER, "Lily Flower");
 
