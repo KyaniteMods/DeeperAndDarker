56a5d668e6b11602bdf400d61d9a900e585a6585
NewJumper <muhtasim.nabi@gmail.com>
2024-08-04 12:20:52 -0500

    no longer need to validate loot tables

--- src/main/java/com/kyanite/deeperdarker/datagen/data/loot/DDLootTableProvider.java
+++ src/main/java/com/kyanite/deeperdarker/datagen/data/loot/DDLootTableProvider.java
@@ -1,25 +1,16 @@
 package com.kyanite.deeperdarker.datagen.data.loot;
 
 import net.minecraft.core.HolderLookup;
-import net.minecraft.core.WritableRegistry;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.loot.LootTableProvider;
-import net.minecraft.util.ProblemReporter;
-import net.minecraft.world.level.storage.loot.BuiltInLootTables;
-import net.minecraft.world.level.storage.loot.LootTable;
-import net.minecraft.world.level.storage.loot.ValidationContext;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
 public class DDLootTableProvider extends LootTableProvider {
     public DDLootTableProvider(PackOutput pOutput, CompletableFuture<HolderLookup.Provider> pRegistries) {
-        super(pOutput, BuiltInLootTables.all(), List.of(new SubProviderEntry(DDBlockLoot::new, LootContextParamSets.BLOCK), new SubProviderEntry(DDChestLoot::new, LootContextParamSets.CHEST), new SubProviderEntry(DDEntityLoot::new, LootContextParamSets.ENTITY)), pRegistries);
-    }
-
-    @Override
-    protected void validate(WritableRegistry<LootTable> registry, ValidationContext context, ProblemReporter.Collector collector) {
-        registry.holders().forEach(lootTable -> lootTable.value().validate(context.setParams(lootTable.value().getParamSet()).enterElement("{" + lootTable.key().location() + "}", lootTable.key())));
+        super(pOutput, Set.of(), List.of(new SubProviderEntry(DDBlockLoot::new, LootContextParamSets.BLOCK), new SubProviderEntry(DDChestLoot::new, LootContextParamSets.CHEST), new SubProviderEntry(DDEntityLoot::new, LootContextParamSets.ENTITY)), pRegistries);
     }
 }
