5b5640d5e67b3ba4ebdc68cbca0bead450faf247
NewJumper <muhtasim.nabi@gmail.com>
2024-08-04 23:43:50 -0500

    no longer need to validate loot tables

--- src/main/java/com/kyanite/deeperdarker/datagen/data/loot/DDLootTableProvider.java
+++ src/main/java/com/kyanite/deeperdarker/datagen/data/loot/DDLootTableProvider.java
@@ -2,21 +2,13 @@ package com.kyanite.deeperdarker.datagen.data.loot;
 
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.loot.LootTableProvider;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.storage.loot.*;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
-import java.util.Map;
+import java.util.Set;
 
 public class DDLootTableProvider extends LootTableProvider {
     public DDLootTableProvider(PackOutput pOutput) {
-        super(pOutput, BuiltInLootTables.all(), List.of(new LootTableProvider.SubProviderEntry(DDBlockLoot::new, LootContextParamSets.BLOCK), new LootTableProvider.SubProviderEntry(DDChestLoot::new, LootContextParamSets.CHEST), new LootTableProvider.SubProviderEntry(DDEntityLoot::new, LootContextParamSets.ENTITY)));
-    }
-
-    @Override
-    protected void validate(Map<ResourceLocation, LootTable> map, @NotNull ValidationContext context) {
-        map.forEach((id, table) -> table.validate(context.setParams(table.getParamSet()).enterElement("{" + id + "}", new LootDataId<>(LootDataType.TABLE, id))));
+        super(pOutput, Set.of(), List.of(new LootTableProvider.SubProviderEntry(DDBlockLoot::new, LootContextParamSets.BLOCK), new LootTableProvider.SubProviderEntry(DDChestLoot::new, LootContextParamSets.CHEST), new LootTableProvider.SubProviderEntry(DDEntityLoot::new, LootContextParamSets.ENTITY)));
     }
 }
