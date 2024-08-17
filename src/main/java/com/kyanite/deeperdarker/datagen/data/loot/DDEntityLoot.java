e7af1ad00c7d7415745e43a63aac4948bb7dba45
NewJumper <muhtasim.nabi@gmail.com>
2024-08-17 17:46:09 -0500

    updated stalker loot table

--- src/main/java/com/kyanite/deeperdarker/datagen/data/loot/DDEntityLoot.java
+++ src/main/java/com/kyanite/deeperdarker/datagen/data/loot/DDEntityLoot.java
@@ -26,12 +26,37 @@ public class DDEntityLoot extends EntityLootSubProvider {
     @Override
     public void generate() {
         add(DDEntities.ANGLER_FISH.get(), LootTable.lootTable());
-        add(DDEntities.SCULK_CENTIPEDE.get(), LootTable.lootTable().withPool(LootPool.lootPool().setRolls(ConstantValue.exactly(1)).add(LootItem.lootTableItem(Items.STRING)).apply(SetItemCountFunction.setCount(UniformGenerator.between(0, 1))).apply(LootingEnchantFunction.lootingMultiplier(UniformGenerator.between(0, 1)))));
-        add(DDEntities.SCULK_LEECH.get(), LootTable.lootTable().withPool(LootPool.lootPool().setRolls(ConstantValue.exactly(1)).add(LootItem.lootTableItem(DDItems.SOUL_DUST.get()).apply(SetItemCountFunction.setCount(UniformGenerator.between(0, 1))).apply(LootingEnchantFunction.lootingMultiplier(UniformGenerator.between(0, 1))))));
-        add(DDEntities.SCULK_SNAPPER.get(), LootTable.lootTable().withPool(LootPool.lootPool().setRolls(ConstantValue.exactly(1)).add(LootItem.lootTableItem(DDItems.SOUL_DUST.get()).apply(SetItemCountFunction.setCount(ConstantValue.exactly(1))).apply(LootingEnchantFunction.lootingMultiplier(UniformGenerator.between(0, 1))))));
-        add(DDEntities.SHATTERED.get(), LootTable.lootTable().withPool(LootPool.lootPool().setRolls(ConstantValue.exactly(1)).add(LootItem.lootTableItem(DDItems.SCULK_BONE.get()).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 3))).apply(LootingEnchantFunction.lootingMultiplier(UniformGenerator.between(0, 1))))));
+        add(DDEntities.SCULK_CENTIPEDE.get(), LootTable.lootTable().withPool(LootPool.lootPool().setRolls(ConstantValue.exactly(1))
+                .add(LootItem.lootTableItem(Items.STRING)
+                        .apply(SetItemCountFunction.setCount(UniformGenerator.between(0, 1)))
+                        .apply(LootingEnchantFunction.lootingMultiplier(UniformGenerator.between(0, 1)))
+                )
+        ));
+        add(DDEntities.SCULK_LEECH.get(), LootTable.lootTable().withPool(LootPool.lootPool().setRolls(ConstantValue.exactly(1))
+                .add(LootItem.lootTableItem(DDItems.SOUL_DUST.get())
+                        .apply(SetItemCountFunction.setCount(UniformGenerator.between(0, 1)))
+                        .apply(LootingEnchantFunction.lootingMultiplier(UniformGenerator.between(0, 1)))
+                )
+        ));
+        add(DDEntities.SCULK_SNAPPER.get(), LootTable.lootTable().withPool(LootPool.lootPool().setRolls(ConstantValue.exactly(1))
+                .add(LootItem.lootTableItem(DDItems.SOUL_DUST.get())
+                        .apply(SetItemCountFunction.setCount(ConstantValue.exactly(1)))
+                        .apply(LootingEnchantFunction.lootingMultiplier(UniformGenerator.between(0, 1)))
+                )
+        ));
+        add(DDEntities.SHATTERED.get(), LootTable.lootTable().withPool(LootPool.lootPool().setRolls(ConstantValue.exactly(1))
+                .add(LootItem.lootTableItem(DDItems.SCULK_BONE.get())
+                        .apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 3)))
+                        .apply(LootingEnchantFunction.lootingMultiplier(UniformGenerator.between(0, 1)))
+                )
+        ));
         add(DDEntities.SHRIEK_WORM.get(), LootTable.lootTable());
-        add(DDEntities.STALKER.get(), LootTable.lootTable().withPool(LootPool.lootPool().setRolls(ConstantValue.exactly(1)).add(LootItem.lootTableItem(DDItems.SOUL_CRYSTAL.get()).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 2))))));
+        add(DDEntities.STALKER.get(), LootTable.lootTable().withPool(LootPool.lootPool().setRolls(ConstantValue.exactly(1))
+                .add(LootItem.lootTableItem(DDItems.SOUL_CRYSTAL.get())
+                        .apply(SetItemCountFunction.setCount(ConstantValue.exactly(1)))
+                        .apply(LootingEnchantFunction.lootingMultiplier(UniformGenerator.between(0, 1)))
+                )
+        ));
     }
 
     @Override
