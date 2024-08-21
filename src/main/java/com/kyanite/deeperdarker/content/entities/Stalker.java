02116e396ecc804994d0f6025a09ab0375c213d5
Pedro <pedro2007.prrr@gmail.com>
2024-08-21 10:56:07 -0300

    Stalkers and shattered can no longer target sculk mobs

--- src/main/java/com/kyanite/deeperdarker/content/entities/Stalker.java
+++ src/main/java/com/kyanite/deeperdarker/content/entities/Stalker.java
@@ -219,7 +219,7 @@ public class Stalker extends Monster implements DisturbanceListener, VibrationSy
 
     public boolean canTargetEntity(Entity target) {
         if(target instanceof LivingEntity entity) {
-            return this.level() == target.level() && EntitySelector.NO_CREATIVE_OR_SPECTATOR.test(target) && !this.isAlliedTo(target) && entity.getType() != EntityType.ARMOR_STAND && entity.getType() != DDEntities.SHATTERED && !entity.isInvulnerable() && !entity.isDeadOrDying() && this.level().getWorldBorder().isWithinBounds(entity.getBoundingBox());
+            return this.level() == target.level() && EntitySelector.NO_CREATIVE_OR_SPECTATOR.test(target) && !this.isAlliedTo(target) && entity.getType() != EntityType.ARMOR_STAND && entity.getMobType() != DDMobType.SCULK && !entity.isInvulnerable() && !entity.isDeadOrDying() && this.level().getWorldBorder().isWithinBounds(entity.getBoundingBox());
         }
 
         return false;
