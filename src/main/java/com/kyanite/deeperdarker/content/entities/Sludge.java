8339b8194a5ef00a87580c117e5b00a2ea29c91f
Pedro <pedro2007.prrr@gmail.com>
2024-08-22 16:21:14 -0300

    Added sludge sound effects

--- src/main/java/com/kyanite/deeperdarker/content/entities/Sludge.java
+++ src/main/java/com/kyanite/deeperdarker/content/entities/Sludge.java
@@ -1,9 +1,12 @@
 package com.kyanite.deeperdarker.content.entities;
 
 import com.kyanite.deeperdarker.content.DDBlocks;
+import com.kyanite.deeperdarker.content.DDSounds;
 import net.minecraft.core.particles.ItemParticleOption;
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
@@ -25,5 +28,36 @@ public class Sludge extends Slime {
     @Override
     protected @NotNull ParticleOptions getParticleType() {
         return new ItemParticleOption(ParticleTypes.ITEM, new ItemStack(DDBlocks.BLOOMING_MOSS_BLOCK));
+    }
+
+    @Override
+    protected SoundEvent getHurtSound(DamageSource damageSource) {
+        if (this.isTiny()) {
+            return DDSounds.SLUDGE_HURT_SMALL;
+        }
+        return DDSounds.SLUDGE_HURT;
+    }
+
+    @Override
+    protected SoundEvent getDeathSound() {
+        if (this.isTiny()) {
+            return DDSounds.SLUDGE_DEATH_SMALL;
+        }
+        return DDSounds.SLUDGE_DEATH;
+    }
+
+    protected SoundEvent getSquishSound() {
+        if (this.isTiny()) {
+            return DDSounds.SLUDGE_SQUISH_SMALL;
+        }
+        return DDSounds.SLUDGE_SQUISH;
+    }
+
+    @Override
+    protected SoundEvent getJumpSound() {
+        if (this.isTiny()) {
+            return DDSounds.SLUDGE_JUMP_SMALL;
+        }
+        return DDSounds.SLUDGE_JUMP;
     }
 }
