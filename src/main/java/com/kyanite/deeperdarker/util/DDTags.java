7872543ec308d062b221ea3922861b40b8486b2a
NewJumper <muhtasim.nabi@gmail.com>
2024-09-27 19:00:17 -0300

    finalized columns gloomslate specific columns still not finished

--- src/main/java/com/kyanite/deeperdarker/util/DDTags.java
+++ src/main/java/com/kyanite/deeperdarker/util/DDTags.java
@@ -16,28 +16,44 @@ import net.minecraft.world.level.levelgen.structure.Structure;
 public class DDTags {
     public static class Blocks {
         public static final TagKey<Block> ECHO_LOGS = TagKey.create(
-                    Registries.BLOCK, DeeperDarker.rl("echo_logs"));
-
+                Registries.BLOCK, DeeperDarker.rl("echo_logs"));
         public static final TagKey<Block> BLOOMING_STEMS = TagKey.create(
                 Registries.BLOCK, DeeperDarker.rl("blooming_stems"));
+
+        public static final TagKey<Block> OTHERSIDE_CARVER_REPLACEABLES = TagKey.create(
+                Registries.BLOCK, DeeperDarker.rl("otherside_carver_replaceables"));
+        public static final TagKey<Block> SCULK_STONE_REPLACEABLES = TagKey.create(
+                Registries.BLOCK, DeeperDarker.rl("sculk_stone_replaceables"));
+        public static final TagKey<Block> SCULK_REPLACEABLES = TagKey.create(
+                Registries.BLOCK, DeeperDarker.rl("sculk_replaceables"));
+        public static final TagKey<Block> DEEPLANDS_COLUMN_REPLACEABLE = TagKey.create(
+                Registries.BLOCK, DeeperDarker.rl("deeplands_column_replaceable"));
+        public static final TagKey<Block> DEEPLANDS_COLUMN_BASE = TagKey.create(
+                Registries.BLOCK, DeeperDarker.rl("deeplands_column_base"));
+
+        public static final TagKey<Block> GLOOMSLATE_REPLACEABLE = TagKey.create(
+                Registries.BLOCK, DeeperDarker.rl("gloomslate_replaceable"));
+        public static final TagKey<Block> GLOOMY_SCULK_REPLACEABLE = TagKey.create(
+                Registries.BLOCK, DeeperDarker.rl("gloomy_sculk_replaceable"));
+        public static final TagKey<Block> OVERCAST_COLUMN_REPLACEABLE = TagKey.create(
+                Registries.BLOCK, DeeperDarker.rl("overcast_column_replaceable"));
+        public static final TagKey<Block> OVERCAST_COLUMN_BASE = TagKey.create(
+                Registries.BLOCK, DeeperDarker.rl("overcast_column_base"));
+
+        public static final TagKey<Block> BLOOMING_POOL_REPLACEABLE = TagKey.create(
+                Registries.BLOCK, DeeperDarker.rl("blooming_pool_replaceable"));
+
+        public static final TagKey<Block> SCULK_VINE_PLACEABLE = TagKey.create(
+                Registries.BLOCK, DeeperDarker.rl("sculk_vine_placeable"));
+        public static final TagKey<Block> GLOWING_VINE_PLACEABLE = TagKey.create(
+                Registries.BLOCK, DeeperDarker.rl("glowing_vine_placeable"));
+
+        public static final TagKey<Block> TRANSMITTABLE = TagKey.create(
+                Registries.BLOCK, DeeperDarker.rl("transmittable"));
+        public static final TagKey<Block> INFINIBURN_OTHERSIDE = TagKey.create(
+                Registries.BLOCK, DeeperDarker.rl("infiniburn_otherside"));
         public static final TagKey<Block> ECHO_SOIL = TagKey.create(
                 Registries.BLOCK, DeeperDarker.rl("echo_soil"));
-        public static final TagKey<Block> SCULK_STONE_REPLACEABLES = TagKey.create(Registries.BLOCK, DeeperDarker.rl("sculk_stone_replaceables"));
-        public static final TagKey<Block> SCULK_REPLACEABLES = TagKey.create(Registries.BLOCK, DeeperDarker.rl("sculk_replaceables"));
-        public static final TagKey<Block> DEEPLANDS_COLUMN_BASE = TagKey.create(Registries.BLOCK, DeeperDarker.rl("deeplands_column_base"));
-        public static final TagKey<Block> GLOOMSLATE_REPLACEABLE = TagKey.create(Registries.BLOCK, DeeperDarker.rl("gloomslate_replaceable"));
-        public static final TagKey<Block> GLOOMY_SCULK_REPLACEABLE = TagKey.create(Registries.BLOCK, DeeperDarker.rl("gloomy_sculk_replaceable"));
-        public static final TagKey<Block> OVERCAST_COLUMN_BASE = TagKey.create(Registries.BLOCK, DeeperDarker.rl("overcast_column_base"));
-        public static final TagKey<Block> BLOOMING_POOL_REPLACEABLE = TagKey.create(Registries.BLOCK, DeeperDarker.rl("glooming_pool_replaceable"));
-
-        public static final TagKey<Block> SCULK_VINE_PLACEABLE = TagKey.create(Registries.BLOCK, DeeperDarker.rl("sculk_vine_placeable"));
-        public static final TagKey<Block> GLOWING_VINE_PLACEABLE = TagKey.create(Registries.BLOCK, DeeperDarker.rl("glowing_vine_placeable"));
-
-        public static final TagKey<Block> TRANSMITTABLE = TagKey.create(Registries.BLOCK, DeeperDarker.rl("transmittable"));
-        public static final TagKey<Block> INFINIBURN_OTHERSIDE = TagKey.create(Registries.BLOCK, DeeperDarker.rl("infiniburn_otherside"));
-        public static final TagKey<Block> INCORRECT_FOR_RESONARIUM_TOOL = TagKey.create(Registries.BLOCK, DeeperDarker.rl("incorrect_for_resonarium_tool"));
-        public static final TagKey<Block> INCORRECT_FOR_WARDEN_TOOL = TagKey.create(Registries.BLOCK, DeeperDarker.rl("incorrect_for_warden_tool"));
-        public static final TagKey<Block> OTHERSIDE_CARVER_REPLACEABLES = TagKey.create(Registries.BLOCK, DeeperDarker.rl("otherside_carver_replaceables"));
     }
 
     public static class Items {
