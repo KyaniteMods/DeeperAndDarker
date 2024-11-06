6a1628fd349659f4a05b19f82c41984aa1cd58ff
NewJumper <muhtasim.nabi@gmail.com>
2024-11-06 17:55:28 -0300

    made a separate resource location for otherside dimension effects fixes #386

--- src/main/java/com/kyanite/deeperdarker/world/otherside/OthersideDimension.java
+++ src/main/java/com/kyanite/deeperdarker/world/otherside/OthersideDimension.java
@@ -8,7 +8,6 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.valueproviders.ConstantInt;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.dimension.BuiltinDimensionTypes;
 import net.minecraft.world.level.dimension.DimensionType;
 
 import java.util.OptionalLong;
@@ -16,8 +15,9 @@ import java.util.OptionalLong;
 public class OthersideDimension {
     public static final ResourceKey<Level> OTHERSIDE_LEVEL = ResourceKey.create(Registries.DIMENSION, new ResourceLocation(DeeperDarker.MOD_ID, "otherside"));
     public static final ResourceKey<DimensionType> OTHERSIDE = ResourceKey.create(Registries.DIMENSION_TYPE, new ResourceLocation(DeeperDarker.MOD_ID, "otherside"));
+    public static final ResourceLocation OTHERSIDE_EFFECTS = new ResourceLocation(DeeperDarker.MOD_ID, "otherside_effects");
 
     public static void bootstrap(BootstapContext<DimensionType> context) {
-        context.register(OTHERSIDE, new DimensionType(OptionalLong.of(18000L), false, true, false, true, 1, true, false, 0, 128, 128, DDTags.Blocks.INFINIBURN_OTHERSIDE, BuiltinDimensionTypes.NETHER_EFFECTS, 0.05f, new DimensionType.MonsterSettings(true, false, ConstantInt.of(7), 15)));
+        context.register(OTHERSIDE, new DimensionType(OptionalLong.of(18000L), false, true, false, true, 1, true, false, 0, 128, 128, DDTags.Blocks.INFINIBURN_OTHERSIDE, OTHERSIDE_EFFECTS, 0.05f, new DimensionType.MonsterSettings(true, false, ConstantInt.of(7), 15)));
     }
 }
