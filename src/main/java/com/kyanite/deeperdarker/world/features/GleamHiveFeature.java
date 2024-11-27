c43714c06317ac28f2d71f812a2567cc8e76d9a9
Pedro <pedro2007.prrr@gmail.com>
2024-11-27 15:29:55 -0300

    Adds outer layer to gleam hive

--- src/main/java/com/kyanite/deeperdarker/world/features/GleamHiveFeature.java
+++ src/main/java/com/kyanite/deeperdarker/world/features/GleamHiveFeature.java
@@ -3,6 +3,7 @@ package com.kyanite.deeperdarker.world.features;
 import com.kyanite.deeperdarker.world.features.config.GleamHiveFeatureConfiguration;
 import com.mojang.serialization.Codec;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.WorldGenLevel;
@@ -11,7 +12,9 @@ import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class GleamHiveFeature extends Feature<GleamHiveFeatureConfiguration> {
     public GleamHiveFeature(Codec<GleamHiveFeatureConfiguration> codec) {
@@ -30,27 +33,43 @@ public class GleamHiveFeature extends Feature<GleamHiveFeatureConfiguration> {
             secondaryRadius = temp;
         }
         BlockPos origin = context.origin().mutable().offset(0, mainRadius, 0);
-        generateBlob(context, origin, mainRadius);
-        generateBlob(context, origin.mutable().offset(context.random().nextInt(mainRadius), context.random().nextInt(mainRadius), context.random().nextInt(mainRadius)), secondaryRadius);
+        HashSet<BlockPos> set = generateBlob(context, origin, mainRadius);
+        set.addAll(generateBlob(context, origin.mutable().offset(context.random().nextInt(mainRadius), context.random().nextInt(mainRadius), context.random().nextInt(mainRadius)), secondaryRadius));
+
+        HashSet<BlockPos> exposed = new HashSet<>();
+        for (BlockPos pos : set) {
+            for (Direction direction : Direction.values()) {
+                if (!set.contains(pos.relative(direction))) exposed.add(pos);
+            }
+        }
+        for (BlockPos pos : exposed) {
+            boolean alternate = random.nextDouble() < context.config().useAlternateBlockChance();
+            if (alternate) {
+                context.level().setBlock(pos, context.config().alternateOuterProvider().getState(random, pos), 2);
+            } else {
+                context.level().setBlock(pos, context.config().outerProvider().getState(random, pos), 2);
+            }
+        }
+
         return true;
     }
 
-    private List<BlockPos> generateBlob(FeaturePlaceContext<GleamHiveFeatureConfiguration> context, BlockPos pos, int radius) {
+    private HashSet<BlockPos> generateBlob(FeaturePlaceContext<GleamHiveFeatureConfiguration> context, BlockPos pos, int radius) {
         WorldGenLevel level = context.level();
         RandomSource random = context.random();
 
-        List<BlockPos> list = new ArrayList<>();
+        HashSet<BlockPos> set = new HashSet<>();
         for (int x = -radius; x <= radius; x++) {
             for (int y = -radius; y <= radius; y++) {
                 for (int z = -radius; z <= radius; z++) {
                     BlockPos newPos = pos.mutable().offset(x, y, z);
                     if (newPos.distSqr(pos) > radius * radius) continue;
                     if (canReplaceBlock(level.getBlockState(newPos))) level.setBlock(newPos, context.config().fillingProvider().getState(random, newPos), 2);
-                    list.add(newPos);
+                    set.add(newPos);
                 }
             }
         }
-        return list;
+        return set;
     }
 
     private boolean canReplaceBlock(BlockState state) {
--- src/main/java/com/kyanite/deeperdarker/world/features/config/GleamHiveFeatureConfiguration.java
+++ src/main/java/com/kyanite/deeperdarker/world/features/config/GleamHiveFeatureConfiguration.java
@@ -6,11 +6,12 @@ import net.minecraft.util.valueproviders.IntProvider;
 import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
 import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
 
-public record GleamHiveFeatureConfiguration(BlockStateProvider fillingProvider, BlockStateProvider outerProvider, BlockStateProvider alternateOuterProvider, IntProvider mainRadiusProvider, IntProvider secondaryRadiusProvider) implements FeatureConfiguration {
+public record GleamHiveFeatureConfiguration(BlockStateProvider fillingProvider, BlockStateProvider outerProvider, BlockStateProvider alternateOuterProvider, double useAlternateBlockChance, IntProvider mainRadiusProvider, IntProvider secondaryRadiusProvider) implements FeatureConfiguration {
     public static final Codec<GleamHiveFeatureConfiguration> CODEC = RecordCodecBuilder.create(instance -> instance.group(
             BlockStateProvider.CODEC.fieldOf("filling_provider").forGetter(f -> f.fillingProvider),
             BlockStateProvider.CODEC.fieldOf("outer_provider").forGetter(f -> f.outerProvider),
             BlockStateProvider.CODEC.fieldOf("alternate_outer_provider").forGetter(f -> f.alternateOuterProvider),
+            Codec.DOUBLE.fieldOf("use_alternate_block_chance").forGetter(f -> f.useAlternateBlockChance),
             IntProvider.CODEC.fieldOf("main_radius_provider").forGetter(f -> f.mainRadiusProvider),
             IntProvider.CODEC.fieldOf("secondary_radius_provider").forGetter(f -> f.secondaryRadiusProvider)
     ).apply(instance, GleamHiveFeatureConfiguration::new));
