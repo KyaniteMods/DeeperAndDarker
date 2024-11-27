b412203228973e98e29ea82e62efef97a55650dd
Pedro <pedro2007.prrr@gmail.com>
2024-11-26 23:33:26 -0300

    Makes heart of the deep beat more based on look angle to the temple

--- src/main/java/com/kyanite/deeperdarker/content/items/HeartOfTheDeepItem.java
+++ src/main/java/com/kyanite/deeperdarker/content/items/HeartOfTheDeepItem.java
@@ -7,8 +7,6 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
-import net.minecraft.util.Mth;
-import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
@@ -17,6 +15,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
 
 public class HeartOfTheDeepItem extends Item {
@@ -26,13 +25,16 @@ public class HeartOfTheDeepItem extends Item {
 
     @Override
     public void inventoryTick(@NotNull ItemStack pItemStack, @NotNull Level pLevel, @NotNull Entity pEntity, int i, boolean bl) {
-        if (pLevel instanceof ServerLevel serverLevel) {
+        if (pLevel instanceof ServerLevel serverLevel && pEntity instanceof Player player && player.isHolding(stack -> stack == pItemStack)) {
             BlockPos pos = serverLevel.findNearestMapStructure(DDTags.Structures.WARDEN_HEART_PULSES, pEntity.blockPosition(), 100, false);
             if (pos != null) {
-                float probability = Mth.sqrt((float) pos.distSqr(pEntity.blockPosition())) == 0 ? 1 : (float) Mth.clamp(1.0f / (Math.log(Mth.sqrt((float) pos.distSqr(pEntity.blockPosition())) * 4.0f + 1)), 0.0f, 1.0f);
-                probability /= 5;
-                if (pEntity instanceof Player player && RandomSource.create().nextFloat() < probability) {
-                    player.playNotifySound(SoundEvents.WARDEN_HEARTBEAT, SoundSource.AMBIENT, 1.7f, 1f);
+                Vec3 vec3i = pos.subtract(pEntity.getOnPos()).getCenter().normalize();
+                double dotProduct = pEntity.getLookAngle().dot(vec3i);
+                if (dotProduct >= 0) {
+                    float period = 10.0f / (float) dotProduct;
+                    if (pLevel.getGameTime() % Math.ceil(period) == 0) {
+                        player.playNotifySound(SoundEvents.WARDEN_HEARTBEAT, SoundSource.AMBIENT, 1.7f, 1f);
+                    }
                 }
             }
         }
