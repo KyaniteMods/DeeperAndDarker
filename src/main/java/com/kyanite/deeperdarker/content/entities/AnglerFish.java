1d6bd4f56671b90f6ac488ec69f8979b571da94e
Pedro <pedro2007.prrr@gmail.com>
2024-12-01 23:17:03 -0300

    Fix crash when trying to bucket angler fish

--- src/main/java/com/kyanite/deeperdarker/content/entities/AnglerFish.java
+++ src/main/java/com/kyanite/deeperdarker/content/entities/AnglerFish.java
@@ -3,6 +3,8 @@ package com.kyanite.deeperdarker.content.entities;
 import com.kyanite.deeperdarker.content.DDSounds;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.AnimationState;
 import net.minecraft.world.entity.Entity;
@@ -19,6 +21,7 @@ import net.minecraft.world.entity.animal.Salmon;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 
@@ -78,7 +81,12 @@ public class AnglerFish extends AbstractFish {
 
     @Override
     public ItemStack getBucketItemStack() {
-        return null;
+        return new ItemStack(Items.WATER_BUCKET);
+    }
+
+    @Override
+    protected InteractionResult mobInteract(Player player, InteractionHand interactionHand) {
+        return InteractionResult.PASS;
     }
 
     static class AnglerFishAttackGoal extends MeleeAttackGoal {
