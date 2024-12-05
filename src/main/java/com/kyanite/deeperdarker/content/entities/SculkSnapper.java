1528586f0828d34d322164b15874ceb0316a4cda
Pedro <pedro2007.prrr@gmail.com>
2024-12-05 11:38:23 -0300

    Sculk snapper can no longer be a child even by editing NBT

--- src/main/java/com/kyanite/deeperdarker/content/entities/SculkSnapper.java
+++ src/main/java/com/kyanite/deeperdarker/content/entities/SculkSnapper.java
@@ -6,6 +6,7 @@ import com.kyanite.deeperdarker.content.DDSounds;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.util.valueproviders.BiasedToBottomInt;
@@ -207,6 +208,12 @@ public class SculkSnapper extends TamableAnimal {
         } else {
             super.handleEntityEvent(pId);
         }
+    }
+
+    @Override
+    public void readAdditionalSaveData(CompoundTag compoundTag) {
+        super.readAdditionalSaveData(compoundTag);
+        this.setAge(Math.max(0, this.getAge()));
     }
 
     @Nullable
