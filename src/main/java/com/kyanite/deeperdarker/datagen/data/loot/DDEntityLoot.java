63e09f55fdb552c8a2838bcd9a6c055bf7ca7903
NewJumper <muhtasim.nabi@gmail.com>
2024-12-11 18:51:17 -0600

    fire aspect now works on angler fish

--- src/main/java/com/kyanite/deeperdarker/datagen/data/loot/DDEntityLoot.java
+++ src/main/java/com/kyanite/deeperdarker/datagen/data/loot/DDEntityLoot.java
@@ -16,6 +16,7 @@ import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.entries.LootItem;
 import net.minecraft.world.level.storage.loot.functions.EnchantedCountIncreaseFunction;
 import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
+import net.minecraft.world.level.storage.loot.functions.SmeltItemFunction;
 import net.minecraft.world.level.storage.loot.predicates.LootItemEntityPropertyCondition;
 import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
 import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
@@ -32,7 +33,9 @@ public class DDEntityLoot extends EntityLootSubProvider {
     @Override
     public void generate() {
         add(DDEntities.ANGLER_FISH.get(), LootTable.lootTable().withPool(LootPool.lootPool().setRolls(ConstantValue.exactly(1))
-                .add(LootItem.lootTableItem(DDItems.ANGLER_FISH.get()))
+                .add(LootItem.lootTableItem(DDItems.ANGLER_FISH.get())
+                        .apply(SmeltItemFunction.smelted().when(this.shouldSmeltLoot()))
+                )
         ));
         add(DDEntities.SCULK_CENTIPEDE.get(), LootTable.lootTable().withPool(LootPool.lootPool().setRolls(ConstantValue.exactly(1))
                 .add(LootItem.lootTableItem(Items.STRING)
