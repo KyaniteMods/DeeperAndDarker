8cd8bb6d33b1d00c221e27be685059505a71bdde
Pedro <pedro2007.prrr@gmail.com>
2025-01-08 12:23:29 -0300

    Fix if statement order in soul elytra overlay

--- src/main/java/com/kyanite/deeperdarker/DeeperDarkerClient.java
+++ src/main/java/com/kyanite/deeperdarker/DeeperDarkerClient.java
@@ -140,13 +140,13 @@ public class DeeperDarkerClient implements ClientModInitializer {
 
         HudRenderCallback.EVENT.register((drawContext, tickDelta) -> {
             Minecraft client = Minecraft.getInstance();
-            if (client.options.hideGui || client.gameMode.getPlayerMode() == GameType.SPECTATOR) {
+            if (client.player == null || DeeperDarker.CONFIG.server.soulElytraCooldown() == -1) return;
+            if (client.options.hideGui || client.gameMode == null || client.gameMode.getPlayerMode() == GameType.SPECTATOR) {
                 return;
             }
 
             ResourceLocation texture = DeeperDarker.rl("textures/gui/soul_elytra_overlay_large.png");
 
-            if (client.player == null || DeeperDarker.CONFIG.server.soulElytraCooldown() == -1) return;
             ItemStack itemStack = client.player.getItemBySlot(EquipmentSlot.CHEST);
             if (itemStack.is(DDItems.SOUL_ELYTRA)) {
                 float f = client.player.getCooldowns().getCooldownPercent(DDItems.SOUL_ELYTRA, (float)(Minecraft.getInstance().getFrameTimeNs() / 1000000000.0));
