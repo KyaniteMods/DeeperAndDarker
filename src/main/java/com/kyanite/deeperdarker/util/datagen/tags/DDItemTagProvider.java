f546fd3d72030bea11cf731d81d6807afdf96567
Pedro <pedro2007.prrr@gmail.com>
2025-01-09 13:41:20 -0300

    Fix warden and resonarium armors not being trimmable

--- src/main/java/com/kyanite/deeperdarker/util/datagen/tags/DDItemTagProvider.java
+++ src/main/java/com/kyanite/deeperdarker/util/datagen/tags/DDItemTagProvider.java
@@ -96,6 +96,8 @@ public class DDItemTagProvider extends FabricTagProvider.ItemTagProvider {
 
         add(ItemTags.NOTE_BLOCK_TOP_INSTRUMENTS, DDItems.SHATTERED_HEAD);
 
+        add(ItemTags.TRIMMABLE_ARMOR, DDItems.WARDEN_HELMET, DDItems.WARDEN_CHESTPLATE, DDItems.WARDEN_LEGGINGS, DDItems.WARDEN_BOOTS, DDItems.RESONARIUM_HELMET, DDItems.RESONARIUM_CHESTPLATE, DDItems.RESONARIUM_LEGGINGS, DDItems.RESONARIUM_BOOTS);
+
         add(ItemTags.FISHES, DDItems.ANGLER_FISH, DDItems.COOKED_ANGLER_FISH);
         add(ConventionalItemTags.FOODS, DDItems.BLOOM_BERRIES, DDItems.ANGLER_FISH, DDItems.COOKED_ANGLER_FISH);
         add(ConventionalItemTags.DUSTS, DDItems.SOUL_DUST);
