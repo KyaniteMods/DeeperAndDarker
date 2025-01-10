5da09769c0d26cde70e4617dc9a03460be5fbca3
NewJumper <muhtasim.nabi@gmail.com>
2025-01-10 12:13:06 -0600

    fixed resonarium armor enchanting

--- src/main/java/com/kyanite/deeperdarker/content/items/ResonariumArmorItem.java
+++ src/main/java/com/kyanite/deeperdarker/content/items/ResonariumArmorItem.java
@@ -1,14 +1,21 @@
 package com.kyanite.deeperdarker.content.items;
 
+import com.kyanite.deeperdarker.util.DDTags;
 import net.minecraft.core.Holder;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantment;
 import org.jetbrains.annotations.NotNull;
 
 public class ResonariumArmorItem extends ArmorItem {
     public ResonariumArmorItem(Holder<ArmorMaterial> material, Type type, Properties properties) {
         super(material, type, properties);
+    }
+
+    @Override
+    public boolean supportsEnchantment(@NotNull ItemStack stack, @NotNull Holder<Enchantment> enchantment) {
+        return !enchantment.is(DDTags.Misc.RESONARIUM_EXCLUDES);
     }
 
     @Override
