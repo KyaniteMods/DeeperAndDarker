a27ad9a3e661c690122029fd6bcbc8c1e2b82034
NewJumper <muhtasim.nabi@gmail.com>
2025-03-10 16:20:54 -0500

    tool tiers' repair ingredients are now stored in a supplier (#439)

--- src/main/java/com/kyanite/deeperdarker/util/DDTiers.java
+++ src/main/java/com/kyanite/deeperdarker/util/DDTiers.java
@@ -7,18 +7,20 @@ import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.block.Block;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.function.Supplier;
+
 public enum DDTiers implements Tier {
-    RESONARIUM(1193, 8, 3, DDTags.Blocks.INCORRECT_FOR_RESONARIUM_TOOL, 13, Ingredient.of(DDItems.RESONARIUM.get())),
-    WARDEN(2519, 10, 5, DDTags.Blocks.INCORRECT_FOR_WARDEN_TOOL, 18, Ingredient.of(DDItems.REINFORCED_ECHO_SHARD.get()));
+    RESONARIUM(1193, 8, 3, DDTags.Blocks.INCORRECT_FOR_RESONARIUM_TOOL, 13, () -> Ingredient.of(DDItems.RESONARIUM.get())),
+    WARDEN(2519, 10, 5, DDTags.Blocks.INCORRECT_FOR_WARDEN_TOOL, 18, () -> Ingredient.of(DDItems.REINFORCED_ECHO_SHARD.get()));
 
     private final int durability;
     private final float speed;
     private final float damage;
     private final TagKey<Block> incorrectBlocks;
     private final int enchantmentValue;
-    private final Ingredient repairIngredient;
+    private final Supplier<Ingredient> repairIngredient;
 
-    DDTiers(int durability, float speed, float damage, TagKey<Block> incorrectBlocks, int enchantmentValue, Ingredient repairIngredient) {
+    DDTiers(int durability, float speed, float damage, TagKey<Block> incorrectBlocks, int enchantmentValue, Supplier<Ingredient> repairIngredient) {
         this.durability = durability;
         this.speed = speed;
         this.damage = damage;
@@ -54,6 +56,6 @@ public enum DDTiers implements Tier {
 
     @Override
     public @NotNull Ingredient getRepairIngredient() {
-        return this.repairIngredient;
+        return this.repairIngredient.get();
     }
 }
