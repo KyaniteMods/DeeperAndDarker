16e46a6a1c375b9f4a75ed6f83ebf252acb8e48b
Pedro <pedro2007.prrr@gmail.com>
2025-11-22 20:47:17 -0300

    Fix slot mixin

--- src/main/java/com/kyanite/deeperdarker/mixin/SlotMixin.java
+++ src/main/java/com/kyanite/deeperdarker/mixin/SlotMixin.java
@@ -1,11 +1,14 @@
 package com.kyanite.deeperdarker.mixin;
 
 import com.kyanite.deeperdarker.content.DDItems;
+import com.kyanite.deeperdarker.content.items.SculkTransmitterItem;
 import net.minecraft.world.Container;
 import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.inventory.CraftingContainer;
 import net.minecraft.world.inventory.PlayerEnderChestContainer;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -18,8 +21,8 @@ public class SlotMixin {
     @Shadow @Final public Container container;
 
     @Inject(method = "mayPlace", at = @At("HEAD"), cancellable = true)
-    private void mayPlace(ItemStack itemStack, CallbackInfoReturnable<Boolean> cir) {
-        if (itemStack.is(DDItems.SCULK_TRANSMITTER) && !(container instanceof Inventory) && !(container instanceof PlayerEnderChestContainer)) {
+    private void mayPlace(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
+        if (stack.getItem() instanceof SculkTransmitterItem && container instanceof BlockEntity) {
             cir.setReturnValue(false);
         }
     }
