35eab9d455620506780f6420bc8f8ece309a2e3e
Pedro <pedro2007.prrr@gmail.com>
2025-11-24 16:55:00 -0300

    Update list of transmittable blocks

--- src/main/java/com/kyanite/deeperdarker/util/datagen/tags/DDBlockTagProvider.java
+++ src/main/java/com/kyanite/deeperdarker/util/datagen/tags/DDBlockTagProvider.java
@@ -125,7 +125,33 @@ public class DDBlockTagProvider extends FabricTagProvider.BlockTagProvider {
         tagBuilder(DDTags.Blocks.SCULK_VINE_PLACEABLE).add(Blocks.SCULK, DDBlocks.SCULK_STONE, DDBlocks.ECHO_LEAVES);
         tagBuilder(DDTags.Blocks.GLOWING_VINE_PLACEABLE).add(Blocks.SCULK, DDBlocks.SCULK_STONE);
 
-        tagBuilder(DDTags.Blocks.TRANSMITTABLE).addOptionalTag(BlockTags.SHULKER_BOXES).addOptionalTag(BlockTags.ANVIL).add(Blocks.CRAFTING_TABLE, Blocks.STONECUTTER, Blocks.CARTOGRAPHY_TABLE, Blocks.SMITHING_TABLE, Blocks.GRINDSTONE, Blocks.LOOM, Blocks.FURNACE, Blocks.SMOKER, Blocks.BLAST_FURNACE, Blocks.CHIPPED_ANVIL, Blocks.ENCHANTING_TABLE, Blocks.BREWING_STAND, Blocks.BEACON, Blocks.CHEST, Blocks.BARREL, Blocks.DISPENSER, Blocks.DROPPER, Blocks.HOPPER, Blocks.TRAPPED_CHEST)
+        tagBuilder(DDTags.Blocks.TRANSMITTABLE)
+                .addOptionalTag(BlockTags.SHULKER_BOXES)
+                .addOptionalTag(BlockTags.ANVIL)
+                .addOptionalTag(ConventionalBlockTags.CHESTS)
+                .addOptionalTag(ConventionalBlockTags.WOODEN_BARRELS)
+                .add(
+                        Blocks.CRAFTING_TABLE,
+                        Blocks.STONECUTTER,
+                        Blocks.CARTOGRAPHY_TABLE,
+                        Blocks.SMITHING_TABLE,
+                        Blocks.GRINDSTONE,
+                        Blocks.LOOM,
+                        Blocks.FURNACE,
+                        Blocks.SMOKER,
+                        Blocks.BLAST_FURNACE,
+                        Blocks.CHIPPED_ANVIL,
+                        Blocks.ENCHANTING_TABLE,
+                        Blocks.BREWING_STAND,
+                        Blocks.BEACON,
+                        Blocks.CHEST,
+                        Blocks.BARREL,
+                        Blocks.DISPENSER,
+                        Blocks.DROPPER,
+                        Blocks.HOPPER,
+                        Blocks.TRAPPED_CHEST,
+                        Blocks.DRAGON_EGG
+                )
                 .addOptional(ResourceLocation.fromNamespaceAndPath("ironchest", "copper_chest"))
                 .addOptional(ResourceLocation.fromNamespaceAndPath("ironchest", "iron_chest"))
                 .addOptional(ResourceLocation.fromNamespaceAndPath("ironchest", "gold_chest"))
