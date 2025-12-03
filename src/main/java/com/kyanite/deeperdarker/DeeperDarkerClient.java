18ed7705668127adfb161fb409a2665d9be15467
Pedro <pedro2007.prrr@gmail.com>
2025-12-03 17:05:43 -0300

    Remove protected sculk gleam translucency

--- src/main/java/com/kyanite/deeperdarker/DeeperDarkerClient.java
+++ src/main/java/com/kyanite/deeperdarker/DeeperDarkerClient.java
@@ -70,7 +70,6 @@ public class DeeperDarkerClient implements ClientModInitializer {
                 DDBlocks.CRYSTALLIZED_AMBER,
                 DDBlocks.SOUNDPROOF_GLASS,
                 DDBlocks.GLOOMSLATE_BARRIER,
-                DDBlocks.PROTECTED_SCULK_GLEAM,
                 DDBlocks.PROTECTED_SCULK_GRIME_GLASS,
                 DDBlocks.SCULK_GRIME_GLASS,
                 DDBlocks.SCULK_GRIME_GLASS_PANE);
