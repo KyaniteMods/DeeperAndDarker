6fe78c67d26ac44f44a28bde0efed0a601d77aed
Pedro <pedro2007.prrr@gmail.com>
2026-03-10 14:02:48 -0300

    Make maze room type registry method return U extends RoomType<T> instead of RoomType<T>

--- src/main/java/com/kyanite/deeperdarker/world/otherside/structures/maze/rooms/RoomTypeRegistry.java
+++ src/main/java/com/kyanite/deeperdarker/world/otherside/structures/maze/rooms/RoomTypeRegistry.java
@@ -17,13 +17,13 @@ public class RoomTypeRegistry {
     public static final ResourceKey<Registry<RoomType<?>>> RESOURCE_KEY = ResourceKey.createRegistryKey(new ResourceLocation(DeeperDarker.MOD_ID, "room_type"));
     public static final Registry<RoomType<?>> REGISTRY = FabricRegistryBuilder.createSimple(RESOURCE_KEY).buildAndRegister();
 
-    public static final SimpleRoomType BLOOMAZE_BOSS_ROOM = (SimpleRoomType) register("bloomaze/boss_room", new SimpleRoomType((options, context, settings, origin, pos, entrance) -> new MazeStructurePieces.BloomazeBossRoomPiece(origin.offset(pos.x() * settings.tileSize(), pos.y() * settings.tileSize(), pos.z() * settings.tileSize()), pos, settings.width(), settings.height(), settings.depth(), settings.tileSize(), settings.palette()), 7, 1, 7));
-    public static final SimpleRoomType GLOOMAZE_BOSS_ROOM = (SimpleRoomType) register("gloomaze/boss_room", new SimpleRoomType((options, context, settings, origin, pos, entrance) -> new MazeStructurePieces.GloomazeBossRoomPiece(origin.offset(pos.x() * settings.tileSize(), pos.y() * settings.tileSize(), pos.z() * settings.tileSize()), pos, settings.width(), settings.height(), settings.depth(), settings.tileSize(), settings.palette()), 7, 1, 7));
-    public static final OakTreeRoomType OAK_TREE_ROOM = (OakTreeRoomType) register("oak_tree_room", new OakTreeRoomType((options, context, settings, origin, pos, entrance) -> new MazeStructurePieces.OakTreeRoomPiece(origin.offset(pos.x() * settings.tileSize(), pos.y() * settings.tileSize(), pos.z() * settings.tileSize()), pos, settings.width(), settings.height(), settings.depth(), settings.tileSize(), settings.palette(), options.leavesLootTable().orElse(null), options.secretLootTable().orElse(null)), 3, 3, 3));
+    public static final SimpleRoomType BLOOMAZE_BOSS_ROOM = register("bloomaze/boss_room", new SimpleRoomType((options, context, settings, origin, pos, entrance) -> new MazeStructurePieces.BloomazeBossRoomPiece(origin.offset(pos.x() * settings.tileSize(), pos.y() * settings.tileSize(), pos.z() * settings.tileSize()), pos, settings.width(), settings.height(), settings.depth(), settings.tileSize(), settings.palette()), 7, 1, 7));
+    public static final SimpleRoomType GLOOMAZE_BOSS_ROOM = register("gloomaze/boss_room", new SimpleRoomType((options, context, settings, origin, pos, entrance) -> new MazeStructurePieces.GloomazeBossRoomPiece(origin.offset(pos.x() * settings.tileSize(), pos.y() * settings.tileSize(), pos.z() * settings.tileSize()), pos, settings.width(), settings.height(), settings.depth(), settings.tileSize(), settings.palette()), 7, 1, 7));
+    public static final OakTreeRoomType OAK_TREE_ROOM = register("oak_tree_room", new OakTreeRoomType((options, context, settings, origin, pos, entrance) -> new MazeStructurePieces.OakTreeRoomPiece(origin.offset(pos.x() * settings.tileSize(), pos.y() * settings.tileSize(), pos.z() * settings.tileSize()), pos, settings.width(), settings.height(), settings.depth(), settings.tileSize(), settings.palette(), options.leavesLootTable().orElse(null), options.secretLootTable().orElse(null)), 3, 3, 3));
 
     public static final Codec<RoomOptions<?>> CODEC = REGISTRY.byNameCodec().dispatch("type", RoomOptions::getType, RoomType::codec);
 
-    public static <T extends RoomOptions<T>> RoomType<T> register(String id, RoomType<T> type) {
+    public static <T extends RoomOptions<T>, U extends RoomType<T>> U register(String id, U type) {
         return Registry.register(REGISTRY, new ResourceLocation(DeeperDarker.MOD_ID, id), type);
     }
 
