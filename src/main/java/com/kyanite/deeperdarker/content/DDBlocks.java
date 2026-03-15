8a5943bf2d5daec9f37eae4f783ed4fba5a8f8ec
Pedro <pedro2007.prrr@gmail.com>
2026-03-15 19:04:00 -0300

    Change strength of maze blocks

--- src/main/java/com/kyanite/deeperdarker/content/DDBlocks.java
+++ src/main/java/com/kyanite/deeperdarker/content/DDBlocks.java
@@ -246,7 +246,7 @@ public class DDBlocks {
     public static final Block CUT_GLOOMSLATE_SLAB = register("cut_gloomslate_slab", new SlabBlock(BlockBehaviour.Properties.copy(CUT_GLOOMSLATE)));
     public static final Block CUT_GLOOMSLATE_WALL = register("cut_gloomslate_wall", new WallBlock(BlockBehaviour.Properties.copy(CUT_GLOOMSLATE)));
     public static final Block CHISELED_GLOOMSLATE = register("chiseled_gloomslate", new Block(BlockBehaviour.Properties.copy(GLOOMSLATE)));
-    public static final Block GLOOMSLATE_BARRIER = register("gloomslate_barrier", new Block(BlockBehaviour.Properties.copy(Blocks.GLASS).strength(2.5f, 4.5f).mapColor(MapColor.TERRACOTTA_BROWN).requiresCorrectToolForDrops()));
+    public static final Block GLOOMSLATE_BARRIER = register("gloomslate_barrier", new Block(BlockBehaviour.Properties.copy(Blocks.GLASS).strength(-1, 3600000.0f).mapColor(MapColor.TERRACOTTA_BROWN).requiresCorrectToolForDrops()));
 
     public static final Block SCULK_GRIME = register("sculk_grime", new MudBlock(BlockBehaviour.Properties.copy(Blocks.SCULK).strength(0.6f).mapColor(MapColor.COLOR_CYAN)));
     public static final Block SCULK_GRIME_BRICKS = register("sculk_grime_bricks", new Block(BlockBehaviour.Properties.copy(Blocks.STONE_BRICKS).mapColor(MapColor.TERRACOTTA_GREEN)));
@@ -298,9 +298,9 @@ public class DDBlocks {
     public static final Block INFESTED_SCULK = register("infested_sculk", new InfestedSculkBlock(Blocks.SCULK, BlockBehaviour.Properties.copy(Blocks.SCULK)));
     public static final Block SCULK_JAW = register("sculk_jaw", new SculkJawBlock(BlockBehaviour.Properties.copy(Blocks.SCULK).randomTicks()));
     public static final Block SOUNDPROOF_GLASS = register("soundproof_glass", new HalfTransparentBlock(BlockBehaviour.Properties.copy(Blocks.TINTED_GLASS)));
-    public static final Block SCULK_LAMP = register("sculk_lamp", new SculkLampBlock(BlockBehaviour.Properties.copy(SCULK_GLEAM).strength(11000.0f).lightLevel(state -> state.getValue(BlockStateProperties.LIT) ? 15 : 0).pushReaction(PushReaction.BLOCK)));
-    public static final Block PROTECTED_SCULK_GLEAM = register("protected_sculk_gleam", new HalfTransparentBlock(BlockBehaviour.Properties.copy(SCULK_GLEAM).strength(11000.0f).pushReaction(PushReaction.BLOCK)));
-    public static final Block PROTECTED_SCULK_GRIME_GLASS = register("protected_sculk_grime_glass", new HalfTransparentBlock(BlockBehaviour.Properties.copy(Blocks.GLASS).strength(11000.0f).pushReaction(PushReaction.BLOCK).mapColor(MapColor.COLOR_CYAN)));
+    public static final Block SCULK_LAMP = register("sculk_lamp", new SculkLampBlock(BlockBehaviour.Properties.copy(SCULK_GLEAM).strength(-1, 3600000.0f).lightLevel(state -> state.getValue(BlockStateProperties.LIT) ? 15 : 0).pushReaction(PushReaction.BLOCK)));
+    public static final Block PROTECTED_SCULK_GLEAM = register("protected_sculk_gleam", new HalfTransparentBlock(BlockBehaviour.Properties.copy(SCULK_GLEAM).strength(-1, 3600000.0f).pushReaction(PushReaction.BLOCK)));
+    public static final Block PROTECTED_SCULK_GRIME_GLASS = register("protected_sculk_grime_glass", new HalfTransparentBlock(BlockBehaviour.Properties.copy(Blocks.GLASS).strength(-1, 3600000.0f).pushReaction(PushReaction.BLOCK).mapColor(MapColor.COLOR_CYAN)));
     public static final Block SCULK_GRIME_GLASS = register("sculk_grime_glass", new HalfTransparentBlock(BlockBehaviour.Properties.copy(Blocks.GLASS).mapColor(MapColor.COLOR_CYAN).strength(0.3f)));
     public static final Block SCULK_GRIME_GLASS_PANE = register("sculk_grime_glass_pane", new IronBarsBlock(BlockBehaviour.Properties.copy(Blocks.GLASS_PANE).mapColor(MapColor.COLOR_CYAN).strength(0.3f)));
     public static final Block RETURN_STATUE = register("return_statue", new ReturnStatueBlock(BlockBehaviour.Properties.copy(Blocks.DEEPSLATE).strength(-1, 3600000.0f).forceSolidOn().noLootTable().pushReaction(PushReaction.BLOCK).isSuffocating(Blocks::never)));
