e44e8919662c52e09a05a990fe018183e0a3115e
Pedro <pedro2007.prrr@gmail.com>
2026-03-15 15:32:37 -0300

    Fix Potty, Pot, and Potter translation keys

--- src/main/java/com/kyanite/deeperdarker/util/datagen/DDENLanguageProvider.java
+++ src/main/java/com/kyanite/deeperdarker/util/datagen/DDENLanguageProvider.java
@@ -311,10 +311,9 @@ public class DDENLanguageProvider extends FabricLanguageProvider {
         translationBuilder.add(DDEntities.SLUDGE, "Sludge");
         translationBuilder.add(DDEntities.SCULK_CENTIPEDE, "Sculk Centipede");
         translationBuilder.add(DDEntities.BLOOMING_GOLEM, "Blooming Golem");
-        translationBuilder.add(DDEntities.POT, "Overcast Pot");
-        translationBuilder.add(DDEntities.POT.getDescriptionId() + ".potty", "Potty");
-        translationBuilder.add(DDEntities.POT.getDescriptionId() + ".pot", "Pot");
-        translationBuilder.add(DDEntities.POT.getDescriptionId() + ".potter", "Potter");
+        translationBuilder.add(DDEntities.POTTY, "Potty");
+        translationBuilder.add(DDEntities.POT, "Pot");
+        translationBuilder.add(DDEntities.POTTER, "Potter");
 
         translationBuilder.add("effect.deeperdarker.sculk_affinity", "Sculk Affinity");
         translationBuilder.add("item.minecraft.tipped_arrow.effect.sculk_affinity", "Arrow of Sculk Affinity");
