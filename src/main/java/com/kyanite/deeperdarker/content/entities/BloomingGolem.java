d5090a9bd25dd2b1f25d7dcbe30ad7f166e9aea5
Pedro <pedro2007.prrr@gmail.com>
2026-03-15 20:18:18 -0300

    Increase blooming golem health to 800

--- src/main/java/com/kyanite/deeperdarker/content/entities/BloomingGolem.java
+++ src/main/java/com/kyanite/deeperdarker/content/entities/BloomingGolem.java
@@ -141,7 +141,7 @@ public class BloomingGolem extends AbstractGolem implements Enemy {
     }
 
     public static AttributeSupplier createAttributes() {
-        return Monster.createMonsterAttributes().add(Attributes.MAX_HEALTH, 500).add(Attributes.ATTACK_DAMAGE, 22).add(Attributes.MOVEMENT_SPEED, 0.3f).add(Attributes.ARMOR, 4).add(Attributes.KNOCKBACK_RESISTANCE, 1).build();
+        return Monster.createMonsterAttributes().add(Attributes.MAX_HEALTH, 800).add(Attributes.ATTACK_DAMAGE, 22).add(Attributes.MOVEMENT_SPEED, 0.3f).add(Attributes.ARMOR, 4).add(Attributes.KNOCKBACK_RESISTANCE, 1).build();
     }
 
     @Override
