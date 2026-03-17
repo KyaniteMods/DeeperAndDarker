bd38a4409b2c0d6da7d4406fc1b69cc97d1f4a97
Pedro <pedro2007.prrr@gmail.com>
2026-03-17 19:22:55 -0300

    Fix `lerpLog` casting to `int` instead of `float`

--- src/main/java/com/kyanite/deeperdarker/util/DDUtil.java
+++ src/main/java/com/kyanite/deeperdarker/util/DDUtil.java
@@ -3,6 +3,6 @@ package com.kyanite.deeperdarker.util;
 public class DDUtil {
     public static float lerpLog(float t, float a, float b) {
         // https://www.cmu.edu/biolphys/deserno/pdf/log_interpol.pdf
-        return (int) (Math.pow(a, t) * Math.pow(b, 1.0f - t));
+        return (float) (Math.pow(a, t) * Math.pow(b, 1.0f - t));
     }
 }
