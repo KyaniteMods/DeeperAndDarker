2c39e846c835bdcec658ff534bb6096e8aa52c20
Pedro <pedro2007.prrr@gmail.com>
2026-03-25 17:14:23 -0300

    Fix shattered head crashing the game

--- src/main/java/com/kyanite/deeperdarker/mixin/ShatteredHeadRenderMixin.java
+++ src/main/java/com/kyanite/deeperdarker/mixin/ShatteredHeadRenderMixin.java
@@ -28,18 +28,13 @@ import java.util.Map;
 @Environment(EnvType.CLIENT)
 @Mixin(SkullBlockRenderer.class)
 public abstract class ShatteredHeadRenderMixin {
-    @Unique
-    private static final ShatteredHeadModel shatteredHeadModel = new ShatteredHeadModel(Minecraft.getInstance().getEntityModels().bakeLayer(DDModelLayers.SHATTERED_HEAD));
-    @Unique
-    private static final ResourceLocation shatteredTexture = new ResourceLocation(DeeperDarker.MOD_ID, "textures/entity/shattered.png");
-
     @Inject(method = "method_3580", at = @At("TAIL"))
     private static void addModel(HashMap<SkullBlock.Type, ResourceLocation> hashMap, CallbackInfo ci) {
-        hashMap.put(ShatteredHeadBlock.SHATTERED, shatteredTexture);
+        hashMap.put(ShatteredHeadBlock.SHATTERED, new ResourceLocation(DeeperDarker.MOD_ID, "textures/entity/shattered.png"));
     }
 
     @Inject(method = "createSkullRenderers", at = @At(value = "INVOKE", target = "Lcom/google/common/collect/ImmutableMap$Builder;build()Lcom/google/common/collect/ImmutableMap;", shift = At.Shift.BEFORE))
     private static void addLayer(EntityModelSet entityModelSet, CallbackInfoReturnable<Map<SkullBlock.Type, SkullModelBase>> cir, @Local(ordinal = 0) ImmutableMap.Builder builder) {
-        builder.put(ShatteredHeadBlock.SHATTERED, shatteredHeadModel);
+        builder.put(ShatteredHeadBlock.SHATTERED, new ShatteredHeadModel(Minecraft.getInstance().getEntityModels().bakeLayer(DDModelLayers.SHATTERED_HEAD)));
     }
 }
