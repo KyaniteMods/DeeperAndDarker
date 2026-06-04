c89aaf90ba42365668798c25095c13cbc8eaedd5
NewJumper <muhtasim.nabi@gmail.com>
2026-06-04 14:17:15 -0500

    bump mod version to 1.4.1

--- src/main/java/com/kyanite/deeperdarker/content/DDLootModifiers.java
+++ src/main/java/com/kyanite/deeperdarker/content/DDLootModifiers.java
@@ -4,11 +4,12 @@ import com.kyanite.deeperdarker.DeeperDarker;
 import com.kyanite.deeperdarker.content.misc.AddItemModifier;
 import com.mojang.serialization.MapCodec;
 import net.neoforged.neoforge.common.loot.IGlobalLootModifier;
-import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.neoforged.neoforge.registries.NeoForgeRegistries;
 
 public class DDLootModifiers {
     public static final DeferredRegister<MapCodec<? extends IGlobalLootModifier>> LOOT_MODIFIERS = DeferredRegister.create(NeoForgeRegistries.GLOBAL_LOOT_MODIFIER_SERIALIZERS, DeeperDarker.MOD_ID);
-    public static final DeferredHolder<MapCodec<? extends IGlobalLootModifier>, MapCodec<AddItemModifier>> ADD = LOOT_MODIFIERS.register("add", () -> AddItemModifier.CODEC);
+    static {
+        LOOT_MODIFIERS.register("add", () -> AddItemModifier.CODEC);
+    }
 }
