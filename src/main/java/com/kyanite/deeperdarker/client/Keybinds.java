788aeddfbc29ac1d3ecba58a59919bfc050127c1
NewJumper <muhtasim.nabi@gmail.com>
2026-06-12 23:07:38 -0500

    fixed keybinds

--- src/main/java/com/kyanite/deeperdarker/client/Keybinds.java
+++ src/main/java/com/kyanite/deeperdarker/client/Keybinds.java
@@ -5,6 +5,8 @@ import net.minecraft.client.KeyMapping;
 import org.lwjgl.glfw.GLFW;
 
 public class Keybinds {
-    public static final KeyMapping BOOST = new KeyMapping("key." + DeeperDarker.MOD_ID + ".boost", GLFW.GLFW_KEY_B, "key.categories." + DeeperDarker.MOD_ID);
-    public static final KeyMapping TRANSMIT = new KeyMapping("key." + DeeperDarker.MOD_ID + ".transmit", GLFW.GLFW_KEY_V, "key.categories." + DeeperDarker.MOD_ID);
+    public static final KeyMapping.Category GAMEPLAY = new KeyMapping.Category(DeeperDarker.rl("gameplay"));
+
+    public static final KeyMapping BOOST = new KeyMapping("key." + DeeperDarker.MOD_ID + ".boost", GLFW.GLFW_KEY_B, GAMEPLAY);
+    public static final KeyMapping TRANSMIT = new KeyMapping("key." + DeeperDarker.MOD_ID + ".transmit", GLFW.GLFW_KEY_V, GAMEPLAY);
 }
